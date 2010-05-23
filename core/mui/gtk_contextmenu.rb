9ec8576840a3f96fa83dca6b95d493b3b57f2e15
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2010-05-23 14:03:57 +0000

    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@65 03aab468-d3d2-4883-8b12-f661bbf03fa8

---
+++ core/mui/gtk_contextmenu.rb
@@ -0,0 +1,29 @@
+
+module Gtk
+  class ContextMenu
+    def initialize(*context)
+      @contextmenu = context end
+
+    def registmenu(label, condition=ret_nth(), &callback)
+      @contextmenu = @contextmenu.push([label, condition, callback]) end
+
+    def registline
+      if block_given?
+        registmenu(nil, lambda{ |*a| yield *a }){ |a,b| }
+      else
+        registmenu(nil){ |a,b| } end end
+
+    def popup(widget, optional)
+      Lock.synchronize{
+        menu = Gtk::Menu.new
+        @contextmenu.each{ |param|
+          label, cond, proc = param
+          if cond.call(optional, widget)
+            if label
+              item = Gtk::MenuItem.new(label)
+              item.signal_connect('activate') { |w| proc.call(optional, widget); false } if proc
+              menu.append(item)
+            else
+              menu.append(Gtk::MenuItem.new) end end }
+        menu.attach_to_widget(widget) {|attach_widgt, mnu| notice "detached" }
+        menu.show_all.popup(nil, nil, 0, 0) } end end end
