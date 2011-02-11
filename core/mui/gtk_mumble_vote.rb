21f051797a489036cd80baf1a5de00aaf0f1ae1e
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2011-02-11 17:48:09 +0000

    ruby1.9.2で起動できない不具合 refs 76
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@239 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/mui/gtk_mumble_vote.rb
+++ core/mui/gtk_mumble_vote.rb
@@ -15,50 +15,52 @@ module Gtk::MumbleVote
   module Voter
     def define_voter(voter, short_name)
       # VOTEしたユーザの配列
+
       by = define_method("#{voter}_by"){
-        @votebuf ||= {}
+        @votebuf ||= Hash.new
         @votebuf[voter] ||= [] }
 
       box = define_method("#{voter}_box"){
-        @vote_box ||= {}
+        @vote_box ||= Hash.new
         @vote_box[voter] ||= Gtk::HBox.new(false, 0) }
 
       label = define_method("#{voter}_label"){
-        @vote_label ||= {}
+        @vote_label ||= Hash.new
         @vote_label[voter] ||= Gtk::Label.new('').set_no_show_all(true) }
 
       gen = define_method("gen_#{voter}"){
-        @gen_voter ||= {}
-        @gen_voter[voter] ||= Gtk::HBox.new(false, 4).closeup(instance_eval(&label)).closeup(instance_eval(&box)).right }
+        @gen_voter ||= Hash.new
+        @gen_voter[voter] ||= Gtk::HBox.new(false, 4).closeup(__send__("#{voter}_label")).closeup(__send__("#{voter}_box")).right }
 
       # _user_ にvoteされたことにする。
       # votebufにユーザを格納し、レンダリングする。
+
       define_method("on_#{voter}"){ |user|
         mainthread_only
         if(UserConfig[:"#{voter}_by_anyone_show_timeline"])
         type_strict user => User
-        if(not instance_eval(&box).destroyed?) and (not instance_eval(&by).include?(user))
-          instance_eval(&box).closeup(Gtk::EventBox.new.add(icon(user, 24)).tooltip(user.idname).show_all)
-          instance_eval(&by) << user
+        if(not __send__("#{voter}_box").destroyed?) and (not __send__("#{voter}_by").include?(user))
+          __send__("#{voter}_box").closeup(Gtk::EventBox.new.add(icon(user, 24)).tooltip(user.idname).show_all)
+          __send__("#{voter}_by") << user
           __send__("rewind_#{voter}_count!") end end }
 
-    # ラベルの数字を書き換える。
-    define_method("rewind_#{voter}_count!"){
-      mainthread_only
-      return if(instance_eval(&box).destroyed?)
-      if(instance_eval(&box).children.size == 0)
-        instance_eval(&label).hide_all.set_no_show_all(true)
-      else
-        instance_eval(&label).set_text("#{instance_eval(&by).size} #{short_name} ").set_no_show_all(false).show_all
-      end
-    }
+      # ラベルの数字を書き換える。
+      define_method("rewind_#{voter}_count!"){
+        mainthread_only
+        return if(__send__("#{voter}_box").destroyed?)
+        if(__send__("#{voter}_box").children.size == 0)
+          __send__("#{voter}_label").hide_all.set_no_show_all(true)
+        else
+          __send__("#{voter}_label").set_text("#{__send__("#{voter}_by").size} #{short_name} ").set_no_show_all(false).show_all
+        end
+      }
 
-    define_method("#{voter}_packer"){
-      unless instance_eval(&by).empty?
-        Delayer.new(Delayer::NORMAL){
-          if(not destroyed?)
-            instance_eval(&by).each{ |user| __send__("on_#{voter}", user) }
-            instance_eval(&gen).show_all end } end }
+      define_method("#{voter}_packer"){
+        unless __send__("#{voter}_by").empty?
+          Delayer.new(Delayer::NORMAL){
+            if(not destroyed?)
+              __send__("#{voter}_by").each{ |user| __send__("on_#{voter}", user) }
+              __send__("gen_#{voter}").show_all end } end }
     end
   end
 
