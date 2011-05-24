6cd2bd7f0d497ab8db2cddb869a7b5fba5f54c0a
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2011-05-24 16:38:13 +0000

    MiraclePainterが更新された時にSegmentation faultする closes 182
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@362 03aab468-d3d2-4883-8b12-f661bbf03fa8

---
+++ core/mui/gtk_pseudo_signal_handler.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+=begin rdoc
+  どんなオブジェクトでも、GLib::Objectのように、signalを発生させたり設定したりできるようにするモジュール。
+=end
+module PseudoSignalHandler
+
+  # シグナル _signal_ に、ハンドラを登録する。シグナルIDを返す。
+  def signal_connect(signal, proc=Proc.new)
+    __signals[signal.to_sym] << proc
+    proc.__id__ end
+
+  # シグナルID _sid_ の登録を解除する
+  def signal_handler_disconnect(sid)
+    __signals.each{ |pair|
+      break if pair.last.reject!{ |handler| handler.__id__ == sid } }
+    self end
+
+  # シグナル _signal_ を発生させる。
+  # シグナルには、第一引数に _self_ 、第二引数以降に _args_ が渡される。
+  def signal_emit(signal, *args)
+    __signals[signal.to_sym].each{ |handler|
+      Delayer.new{
+        if not destroyed?
+          handler.call(*[self, *args][0, handler.arity]) end } }
+    self end
+
+  private
+
+  def __signals
+    @__signals ||= Hash.new{ |h, k| h[k] = [] } end
+
+end
