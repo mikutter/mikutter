6974342857068074dd2e405b6a4e0f5fbfcf10f0
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2011-05-28 05:19:31 +0000

    ruby-hmac 0.4.0をlib/に追加。
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@371 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/lib/oauth-0.3.6/lib/oauth.rb
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-require 'oauth/oauth'
-require 'oauth/client/helper'
-require 'oauth/signature/hmac/sha1'
-require 'oauth/request_proxy/mock_request'
--- core/lib/oauth-0.3.6/lib/oauth/errors.rb
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-require 'oauth/errors/error'
-require 'oauth/errors/unauthorized'
-require 'oauth/errors/problem'
