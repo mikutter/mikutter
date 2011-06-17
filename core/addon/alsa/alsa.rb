e895b49ece360163b117a846ab7eeea8b06a9098
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2011-06-17 18:29:18 +0000

    非ubuntu環境でサウンドが鳴らない closes 236
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@413 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/addon/alsa/alsa.rb
+++ core/addon/alsa/alsa.rb
@@ -3,9 +3,9 @@
 
 Module.new do
 
-  if command_exist? "alsa"
+  if command_exist? "aplay"
     Plugin::create(:alsa).add_event(:play_sound){ |filename, &stop|
-      bg_system("aplay","-q", filename)
+      bg_system("aplay","-q", filename) if FileTest.exist?(filename)
       stop.call
     }
   end
