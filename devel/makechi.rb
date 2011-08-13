fe794cd21e012c5346aa8e215e59bbdd27e191a1
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2011-08-13 18:14:22 +0000

    プラグインを削除したさらの状態のmikutterを作るスクリプト refs #274
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@458 03aab468-d3d2-4883-8b12-f661bbf03fa8

---
+++ devel/makechi.rb
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+=begin rdoc
+  Mikutterのプラグインをすべて削除した空のCHIを作成する
+=end
+
+require 'fileutils'
+
+def get_config_data(name)
+  case name
+  when "NAME"
+    "chi"
+  when "ACRO"
+    "chi"
+  else
+    CHIConfig.const_get(name) end end
+
+BASE = File.expand_path(File.dirname($0))
+SRC = File.expand_path(File.join(File.dirname($0), '..'))
+DEST = File.expand_path(File.join(File.dirname($0), 'src'))
+
+Dir.chdir(BASE)
+
+if FileTest.exist?(DEST)
+  FileUtils.rm_rf DEST
+  puts "directory #{DEST} already exist."
+end
+
+FileUtils.mkdir_p File.join(DEST, 'plugin')
+FileUtils.cp File.join(SRC, 'mikutter.rb'), DEST
+FileUtils.cp_r File.join(SRC, 'core'), DEST
+FileUtils.copy File.join(SRC, 'mikutter.rb'), File.join(SRC, 'chi.rb')
+FileUtils.rm_rf File.join(DEST, 'core', 'plugin', 'gui.rb')
+FileUtils.rm_rf Dir.glob(File.join(DEST, 'core', 'addon', '*'))
+
+Dir.chdir(File.join(DEST, 'core'))
+require 'config'
+Dir.chdir(BASE)
+
+open(File.join(DEST, "core/config.rb"), 'w'){ |out|
+  out.write([ '# -*- coding: utf-8 -*-','','module CHIConfig',
+              CHIConfig.constants.map{ |name|
+                "  #{name} = #{get_config_data(name).inspect}"
+              },
+              'end'].join("\n")) }
