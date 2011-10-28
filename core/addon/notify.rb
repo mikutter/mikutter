aac66833c679e3b4b1ff2a02b74ed62dfb0e7fc1
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2011-10-28 18:19:19 +0000

    通知プラグインを新しい書き方に書きなおした。設定画面をDSLで作れるようにした closes #331
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@562 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/addon/notify.rb
+++ core/addon/notify.rb
@@ -3,55 +3,28 @@ miquire :addon, 'addon'
 miquire :core, 'environment'
 miquire :addon, 'settings'
 
-#require 'gst'
-
-Module.new do
+Plugin.create(:notify) do
 
   DEFAULT_SOUND_DIRECTORY = 'skin/data/sounds'
   DEFINED_TIME = Time.new.freeze
 
-  def self.boot
-    plugin = Plugin::create(:notify)
+  settings("通知") do
+    def self.def_notify(label, kind)
+      settings (label) do
+        boolean 'ポップアップ', :"notify_#{kind}"
+      fileselect('サウンド', :"notify_sound_#{kind}", DEFAULT_SOUND_DIRECTORY) end end
 
-    plugin.add_event(:boot){ |service|
-      Plugin.call(:setting_tab_regist, main, '通知') }
-    plugin.add_event(:update, &method(:onupdate))
-    plugin.add_event(:mention, &method(:onmention))
-    plugin.add_event(:mention, &method(:onretweeted))
-    plugin.add_event(:followers_created, &method(:onfollowed))
-    plugin.add_event(:followers_destroy, &method(:onremoved))
-    plugin.add_event(:favorite, &method(:onfavorited))
-    plugin.add_event(:direct_messages, &method(:ondm))
-  end
+  def_notify "フレンドタイムライン", :friend_timeline
+  def_notify "リプライ", :mention
+  def_notify 'フォローされたとき', :followed
+  def_notify 'フォロー解除されたとき', :removed
+  def_notify 'リツイートされたとき', :retweeted
+  def_notify 'ふぁぼられたとき', :favorited
+  def_notify 'ダイレクトメッセージ受信', :direct_message
+  adjustment('通知を表示し続ける秒数', :notify_expire_time, 1, 60)
+end
 
-  def self.main
-    box = Gtk::VBox.new(false, 8)
-    ft = Mtk.group('フレンドタイムライン',
-                   Mtk.boolean(:notify_friend_timeline, 'ポップアップ'),
-                   Mtk.fileselect(:notify_sound_friend_timeline, 'サウンド', DEFAULT_SOUND_DIRECTORY))
-    me = Mtk.group('リプライ',
-                   Mtk.boolean(:notify_mention, 'ポップアップ'),
-                   Mtk.fileselect(:notify_sound_mention, 'サウンド', DEFAULT_SOUND_DIRECTORY))
-    fd = Mtk.group('フォローされたとき',
-                   Mtk.boolean(:notify_followed, 'ポップアップ'),
-                   Mtk.fileselect(:notify_sound_followed, 'サウンド', DEFAULT_SOUND_DIRECTORY))
-    rd = Mtk.group('フォロー解除されたとき',
-                   Mtk.boolean(:notify_removed, 'ポップアップ'),
-                   Mtk.fileselect(:notify_sound_removed, 'サウンド', DEFAULT_SOUND_DIRECTORY))
-    rt = Mtk.group('リツイートされたとき',
-                   Mtk.boolean(:notify_retweeted, 'ポップアップ'),
-                   Mtk.fileselect(:notify_sound_retweeted, 'サウンド', DEFAULT_SOUND_DIRECTORY))
-    fv = Mtk.group('ふぁぼられたとき',
-                   Mtk.boolean(:notify_favorited, 'ポップアップ'),
-                   Mtk.fileselect(:notify_sound_favorited, 'サウンド', DEFAULT_SOUND_DIRECTORY))
-    dm = Mtk.group('ダイレクトメッセージ受信',
-                   Mtk.boolean(:notify_direct_message, 'ポップアップ'),
-                   Mtk.fileselect(:notify_sound_direct_message, 'サウンド', DEFAULT_SOUND_DIRECTORY))
-    box.closeup(ft).closeup(me).closeup(fd).closeup(rd).closeup(rt).closeup(fv).closeup(dm)
-    box.pack_start(Mtk.adjustment('通知を表示し続ける秒数', :notify_expire_time, 1, 60), false)
-  end
-
-  def self.onupdate(post, raw_messages)
+  onupdate do |post, raw_messages|
     messages = Plugin.filtering(:show_filter, raw_messages.select{ |m| not(m.from_me? or m.to_me?) and m[:created] > DEFINED_TIME }).first
     if not(messages.empty?)
       if(UserConfig[:notify_friend_timeline])
@@ -60,7 +33,7 @@ Module.new do
       if(UserConfig[:notify_sound_friend_timeline])
         self.notify_sound(UserConfig[:notify_sound_friend_timeline]) end end end
 
-  def self.onmention(post, raw_messages)
+  onmention do |post, raw_messages|
     messages = Plugin.filtering(:show_filter, raw_messages.select{ |m| not(m.from_me? or m[:retweet]) and m[:created] > DEFINED_TIME }).first
     if not(messages.empty?)
       if(not(UserConfig[:notify_friend_timeline]) and UserConfig[:notify_mention])
@@ -69,7 +42,7 @@ Module.new do
       if(UserConfig[:notify_sound_mention])
         self.notify_sound(UserConfig[:notify_sound_mention]) end end end
 
-  def self.onfollowed(post, users)
+  on_followers_created do |post, users|
     if not(users.empty?)
       if(UserConfig[:notify_followed])
         users.each{ |user|
@@ -77,21 +50,21 @@ Module.new do
       if(UserConfig[:notify_sound_followed])
         self.notify_sound(UserConfig[:notify_sound_followed]) end end end
 
-  def self.onremoved(post, users)
+  on_followers_destroy do |post, users|
     if not(users.empty?)
       if(UserConfig[:notify_removed])
         self.notify(users.first, users.map{|u| "@#{u[:idname]}" }.join(' ')+' にリムーブされました。') end
       if(UserConfig[:notify_sound_removed])
         self.notify_sound(UserConfig[:notify_sound_removed]) end end end
 
-  def self.onfavorited(service, by, to)
+  on_favorite do |service, by, to|
     if to.from_me?
       if(UserConfig[:notify_favorited])
         self.notify(by, "fav by #{by[:idname]} \"#{to.to_s}\"") end
       if(UserConfig[:notify_sound_favorited])
         self.notify_sound(UserConfig[:notify_sound_favorited]) end end end
 
-  def self.onretweeted(post, raw_messages)
+  onmention do |post, raw_messages|
     messages = Plugin.filtering(:show_filter, raw_messages.select{ |m| m[:retweet] and not m.from_me? }).first
     if not(messages.empty?)
       if(UserConfig[:notify_retweeted])
@@ -100,7 +73,7 @@ Module.new do
       if(UserConfig[:notify_sound_retweeted])
         self.notify_sound(UserConfig[:notify_sound_retweeted]) end end end
 
-  def self.ondm(post, dms)
+  on_direct_messages do |post, dms|
     newer_dms = dms.select{ |dm| Time.parse(dm[:created_at]) > DEFINED_TIME }
     if not(newer_dms.empty?)
       if(UserConfig[:notify_direct_message])
@@ -116,5 +89,4 @@ Module.new do
     if sndfile.respond_to?(:to_s) and FileTest.exist?(sndfile.to_s)
       Plugin.call(:play_sound, sndfile) end end
 
-  boot
 end
