a77def3b5bcbcc616a3039970ee3b0009bd58d6f
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2011-10-29 11:03:08 +0000

    基本設定をDSLで書きなおし、クレジットボタンを実装 refs #331
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@563 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/addon/settings/builder.rb
+++ core/addon/settings/builder.rb
@@ -1,10 +1,29 @@
 # -*- coding: utf-8 -*-
 
+=begin rdoc
+プラグインに、簡単に設定ファイルを定義する機能を提供する。
+以下の例は、このクラスを利用してプラグインの設定画面を定義する例。
+  Plugin.create(:test) do
+    settings("設定") do
+      boolean "チェックする", :test_check
+    end
+  end
+
+settingsの中身は、 Plugin::Setting のインスタンスの中で実行される。
+つまり、 Plugin::Setting のインスタンスメソッドは、 _settings{}_ の中で実行できるメソッドと同じです。
+例ではbooleanメソッドを呼び出して、真偽値を入力させるウィジェットを配置させるように定義している
+(チェックボックス)。明確にウィジェットを設定できるわけではなくて、値の意味を定義するだけなので、
+前後関係などに影響されてウィジェットが変わる場合があるかも。
+=end
 class Plugin::Setting < Gtk::VBox
-  def multitext(label, config, optional=nil)
+
+  # 複数行テキスト
+  # ==== Args
+  # [label] ラベル
+  # [config] 設定のキー
+  def multitext(label, config)
     container = Gtk::HBox.new(false, 0)
     input = Gtk::TextView.new
-    input.tooltip optional if optional
     input.wrap_mode = Gtk::TextTag::WRAP_CHAR
     input.border_width = 2
     input.accepts_tab = false
@@ -16,8 +35,15 @@ class Plugin::Setting < Gtk::VBox
     input.buffer.ssc('changed'){ |widget|
       UserConfig[config] = widget.text }
     closeup container
+    container
   end
 
+  # 特定範囲の数値入力
+  # ==== Args
+  # [label] ラベル
+  # [config] 設定のキー
+  # [min] 最低値。これより小さい数字は入力できないようになる
+  # [max] 最高値。これより大きい数字は入力できないようになる
   def adjustment(name, config, min, max)
     container = Gtk::HBox.new(false, 0)
     container.pack_start(Gtk::Label.new(name), false, true, 0)
@@ -29,15 +55,26 @@ class Plugin::Setting < Gtk::VBox
       false
     }
     closeup container.pack_start(Gtk::Alignment.new(1.0, 0.5, 0, 0).add(spinner), true, true, 0)
+    container
   end
 
+  # 真偽値入力
+  # ==== Args
+  # [label] ラベル
+  # [config] 設定のキー
   def boolean(label, key)
     input = Gtk::CheckButton.new(label)
     input.active = UserConfig[key]
     input.signal_connect('toggled'){ |widget|
       UserConfig[key] = widget.active? }
-    closeup input end
+    closeup input
+    input end
 
+  # ファイルを選択する
+  # ==== Args
+  # [label] ラベル
+  # [config] 設定のキー
+  # [current] 初期のディレクトリ
   def fileselect(label, key, current=Dir.pwd)
     container = input = nil
     Mtk.input(key, label){ |c, i|
@@ -60,8 +97,13 @@ class Plugin::Setting < Gtk::VBox
       dialog.destroy
     }
     closeup container
+    container
   end
 
+  # 設定のグループ。関連の強い設定をカテゴライズできる。
+  # ==== Args
+  # [title] ラベル
+  # [&block] ブロック
   def settings(title)
     group = Gtk::Frame.new.set_border_width(8)
     if(title.is_a?(Gtk::Widget))
@@ -71,6 +113,37 @@ class Plugin::Setting < Gtk::VBox
     box = Plugin::Setting.new.set_border_width(4)
     box.instance_eval(&Proc.new)
     closeup group.add(box)
+    group
   end
+
+  # 〜についてダイアログを出すためのボタン。押すとダイアログが出てくる
+  # ==== Args
+  # [label] ラベル
+  # [options]
+  #   設定値。以下のキーを含むハッシュ。
+  #   _:name_ :: ソフトウェア名
+  #   _:version_ :: バージョン
+  #   _:copyright_ :: コピーライト
+  #   _:comments_ :: コメント
+  #   _:license_ :: ライセンス
+  #   _:website_ :: Webページ
+  #   _:logo_ :: ロゴ画像のフルパス
+  #   _:authors_ :: 作者の名前。通常Twitter screen name（Array）
+  #   _:artists_ :: デザイナとかの名前。通常Twitter screen name（Array）
+  #   _:documenters_ :: ドキュメントかいた人とかの名前。通常Twitter screen name（Array）
+  def about(label, options={})
+    about = Gtk::Button.new("#{Environment::NAME} について")
+    about.signal_connect("clicked"){
+      dialog = Gtk::AboutDialog.new.show
+      options.each { |key, value|
+        dialog.__send__("#{key}=", about_converter[key][value]) }
+      dialog.signal_connect('response') { dialog.destroy } }
+    closeup about
+    about end
+
+  private
+  def about_converter
+    Hash.new(ret_nth).merge!( :logo => lambda{ |value| Gtk::WebIcon.new(value).pixbuf rescue nil } ) end
+  memoize :about_converter
 
 end
--- core/addon/settings/settings.rb
+++ core/addon/settings/settings.rb
@@ -6,6 +6,7 @@ miquire :mui, 'skin'
 miquire :mui, 'mtk'
 
 require File.expand_path File.join(File.dirname(__FILE__), 'builder')
+require File.expand_path File.join(File.dirname(__FILE__), 'basic_settings')
 
 Plugin::create(:settings) do
   book = Gtk::Notebook.new.set_tab_pos(Gtk::POS_TOP).set_scrollable(true)
@@ -32,74 +33,74 @@ Plugin::create(:settings) do
 
 end
 
-Module.new do
-  plugin = Plugin::create(:basic_settings)
-  box = Gtk::VBox.new(false, 0)
-  retrieve_interval = Mtk.group('各情報を取りに行く間隔。単位は分',
-                                Mtk.adjustment('タイムラインとリプライ',
-                                               :retrieve_interval_friendtl, 1, 60*24).
-                                tooltip('あなたがフォローしている人からのリプライとつぶやきの取得間隔'),
-                                Mtk.adjustment('フォローしていない人からのリプライ',
-                                               :retrieve_interval_mention, 1, 60*24).
-                                tooltip("あなたに送られてきたリプライを取得する間隔。\n上との違いは、あなたがフォローしていない人からのリプライも取得出来ることです"),
-                                Mtk.adjustment('保存した検索',
-                                               :retrieve_interval_search, 1, 60*24).
-                                tooltip('保存した検索を確認しに行く間隔'),
-                                Mtk.adjustment('フォロー',
-                                               :retrieve_interval_followings, 1, 60*24).
-                                tooltip('フォロー一覧を確認しに行く間隔。mikutterを使わずにフォローした場合、この時に同期される'),
-                                Mtk.adjustment('フォロワー',
-                                               :retrieve_interval_followers, 1, 60*24).
-                                tooltip('フォロワー一覧を確認しに行く間隔'),
-                                Mtk.adjustment('ダイレクトメッセージ',
-                                               :retrieve_interval_direct_messages, 1, 60*24).
-                                tooltip('ダイレクトメッセージを確認しに行く間隔'))
-  retrieve_count = Gtk::Frame.new('一度に取得するつぶやきの件数(1-200)').set_border_width(8)
-  rcbox = Gtk::VBox.new(false, 0).set_border_width(4)
-  retrieve_count.add(rcbox)
-  rcbox.pack_start(Mtk.adjustment('タイムラインとリプライ', :retrieve_count_friendtl, 1, 200), false)
-  rcbox.pack_start(Mtk.adjustment('フォローしていない人からのリプライ', :retrieve_count_mention, 1, 200), false)
-  rcbox.pack_start(Mtk.adjustment('フォロー', :retrieve_count_followings, 1, 100000), false)
-  rcbox.pack_start(Mtk.adjustment('フォロワー', :retrieve_count_followers, 1, 100000), false)
-  rcbox.pack_start(Mtk.adjustment('ダイレクトメッセージ', :retrieve_count_direct_messages, 1, 200), false)
+# Module.new do
+#   plugin = Plugin::create(:basic_settings)
+#   box = Gtk::VBox.new(false, 0)
+#   retrieve_interval = Mtk.group('各情報を取りに行く間隔。単位は分',
+#                                 Mtk.adjustment('タイムラインとリプライ',
+#                                                :retrieve_interval_friendtl, 1, 60*24).
+#                                 tooltip('あなたがフォローしている人からのリプライとつぶやきの取得間隔'),
+#                                 Mtk.adjustment('フォローしていない人からのリプライ',
+#                                                :retrieve_interval_mention, 1, 60*24).
+#                                 tooltip("あなたに送られてきたリプライを取得する間隔。\n上との違いは、あなたがフォローしていない人からのリプライも取得出来ることです"),
+#                                 Mtk.adjustment('保存した検索',
+#                                                :retrieve_interval_search, 1, 60*24).
+#                                 tooltip('保存した検索を確認しに行く間隔'),
+#                                 Mtk.adjustment('フォロー',
+#                                                :retrieve_interval_followings, 1, 60*24).
+#                                 tooltip('フォロー一覧を確認しに行く間隔。mikutterを使わずにフォローした場合、この時に同期される'),
+#                                 Mtk.adjustment('フォロワー',
+#                                                :retrieve_interval_followers, 1, 60*24).
+#                                 tooltip('フォロワー一覧を確認しに行く間隔'),
+#                                 Mtk.adjustment('ダイレクトメッセージ',
+#                                                :retrieve_interval_direct_messages, 1, 60*24).
+#                                 tooltip('ダイレクトメッセージを確認しに行く間隔'))
+#   retrieve_count = Gtk::Frame.new('一度に取得するつぶやきの件数(1-200)').set_border_width(8)
+#   rcbox = Gtk::VBox.new(false, 0).set_border_width(4)
+#   retrieve_count.add(rcbox)
+#   rcbox.pack_start(Mtk.adjustment('タイムラインとリプライ', :retrieve_count_friendtl, 1, 200), false)
+#   rcbox.pack_start(Mtk.adjustment('フォローしていない人からのリプライ', :retrieve_count_mention, 1, 200), false)
+#   rcbox.pack_start(Mtk.adjustment('フォロー', :retrieve_count_followings, 1, 100000), false)
+#   rcbox.pack_start(Mtk.adjustment('フォロワー', :retrieve_count_followers, 1, 100000), false)
+#   rcbox.pack_start(Mtk.adjustment('ダイレクトメッセージ', :retrieve_count_direct_messages, 1, 200), false)
 
-  queue_interval_count = Gtk::Frame.new('イベントの発生頻度(ミリ秒単位)').set_border_width(8)
-  qibox = Gtk::VBox.new(false, 0).set_border_width(4)
-  queue_interval_count.add(qibox)
-  qibox.pack_start(Mtk.adjustment('タイムラインとリプライとリツイート', :update_queue_delay, 1, 1000), false)
-  qibox.pack_start(Mtk.adjustment('ふぁぼられ', :favorite_queue_delay, 1, 1000), false)
-  qibox.pack_start(Mtk.adjustment('フォロワー', :follow_queue_delay, 1, 1000), false)
-  qibox.pack_start(Mtk.adjustment('ダイレクトメッセージ', :direct_message_queue_delay, 1, 1000), false)
+#   queue_interval_count = Gtk::Frame.new('イベントの発生頻度(ミリ秒単位)').set_border_width(8)
+#   qibox = Gtk::VBox.new(false, 0).set_border_width(4)
+#   queue_interval_count.add(qibox)
+#   qibox.pack_start(Mtk.adjustment('タイムラインとリプライとリツイート', :update_queue_delay, 1, 1000), false)
+#   qibox.pack_start(Mtk.adjustment('ふぁぼられ', :favorite_queue_delay, 1, 1000), false)
+#   qibox.pack_start(Mtk.adjustment('フォロワー', :follow_queue_delay, 1, 1000), false)
+#   qibox.pack_start(Mtk.adjustment('ダイレクトメッセージ', :direct_message_queue_delay, 1, 1000), false)
 
-  box.pack_start(retrieve_interval, false)
-  box.pack_start(retrieve_count, false)
-  box.pack_start(queue_interval_count, false)
-  box.pack_start(Mtk.boolean(:retrieve_force_mumbleparent, 'リプライ元をサーバに問い合わせて取得する'), false)
-  box.pack_start(Mtk.boolean(:anti_retrieve_fail, 'つぶやきの取得漏れを防止する（遅延対策）'), false)
-  box.pack_start(Gtk::Label.new('遅延に強くなりますが、ちょっと遅くなります。'), false)
-  box.pack_start(Mtk.boolean(:realtime_rewind, 'リアルタイム更新'), false)
-  about = Gtk::Button.new("#{Environment::NAME} について")
-  about.signal_connect("clicked"){
-    dialog = Gtk::AboutDialog.new.show
-    dialog.name = dialog.program_name = Environment::NAME
-    dialog.version = Environment::VERSION.to_s
-    dialog.copyright = '2009-2011 Toshiaki Asai'
-    dialog.comments = "全てのミク廃、そしてTwitter中毒者へ贈る、至高のTwitter Clientを目指すTwitter Client。
-略して至高のTwitter Client。
-圧倒的なかわいさではないか我がミクは
+#   box.pack_start(retrieve_interval, false)
+#   box.pack_start(retrieve_count, false)
+#   box.pack_start(queue_interval_count, false)
+#   box.pack_start(Mtk.boolean(:retrieve_force_mumbleparent, 'リプライ元をサーバに問い合わせて取得する'), false)
+#   box.pack_start(Mtk.boolean(:anti_retrieve_fail, 'つぶやきの取得漏れを防止する（遅延対策）'), false)
+#   box.pack_start(Gtk::Label.new('遅延に強くなりますが、ちょっと遅くなります。'), false)
+#   box.pack_start(Mtk.boolean(:realtime_rewind, 'リアルタイム更新'), false)
+#   about = Gtk::Button.new("#{Environment::NAME} について")
+#   about.signal_connect("clicked"){
+#     dialog = Gtk::AboutDialog.new.show
+#     dialog.name = dialog.program_name = Environment::NAME
+#     dialog.version = Environment::VERSION.to_s
+#     dialog.copyright = '2009-2011 Toshiaki Asai'
+#     dialog.comments = "全てのミク廃、そしてTwitter中毒者へ贈る、至高のTwitter Clientを目指すTwitter Client。
+# 略して至高のTwitter Client。
+# 圧倒的なかわいさではないか我がミクは
 
-このソフトウェアは GPL3 によって浄化されています。"
-    dialog.license = file_get_contents('../LICENSE') rescue nil
-    dialog.website = 'http://mikutter.hachune.net/'
-    dialog.logo = Gtk::WebIcon.new(MUI::Skin.get('icon.png')).pixbuf rescue nil
-    dialog.authors = ['toshi_a', 'Phenomer', 'tana_ash']
-    dialog.artists = ['toshi_a', 'soramame_bscl', 'bina1204', 'seibe2']
-    dialog.documenters = ['toshi_a']
-    dialog.signal_connect('response') { dialog.destroy } }
-  box.closeup(about.right)
+# このソフトウェアは GPL3 によって浄化されています。"
+#     dialog.license = file_get_contents('../LICENSE') rescue nil
+#     dialog.website = 'http://mikutter.hachune.net/'
+#     dialog.logo = Gtk::WebIcon.new(MUI::Skin.get('icon.png')).pixbuf rescue nil
+#     dialog.authors = ['toshi_a', 'Phenomer', 'tana_ash']
+#     dialog.artists = ['toshi_a', 'soramame_bscl', 'bina1204', 'seibe2']
+#     dialog.documenters = ['toshi_a']
+#     dialog.signal_connect('response') { dialog.destroy } }
+#   box.closeup(about.right)
 
-  plugin.add_event(:boot){ |service|
-    Plugin.call(:setting_tab_regist, box, '基本設定') }
+#   plugin.add_event(:boot){ |service|
+#     Plugin.call(:setting_tab_regist, box, '基本設定') }
 
 
-end
+# end
