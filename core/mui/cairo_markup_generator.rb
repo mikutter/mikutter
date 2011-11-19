491f87f16698c31348498377d5d3e3606ced5f6e
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2011-11-19 08:32:43 +0000

    エスケープ処理にバグがあったのでとりあえず正常に動作するようにした refs #368 @2
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@588 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/mui/cairo_markup_generator.rb
+++ core/mui/cairo_markup_generator.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+require 'gtk2'
+
 module Pango
   ESCAPE_RULE = {'&' => '&amp;' ,'>' => '&gt;', '<' => '&lt;'}.freeze
   class << self
@@ -24,7 +26,7 @@ module Pango
 
 module Gdk::MarkupGenerator
 
-  ESCAPE_KEYS = Regexp::union(*Pango::ESCAPE_RULE.keys.freeze)
+  ESCAPE_KEYS = Regexp::union(*Pango::ESCAPE_RULE.keys)
   ESCAPE_KV = Pango::ESCAPE_RULE.method(:[])
 
   # 本文を返す
@@ -38,20 +40,15 @@ module Gdk::MarkupGenerator
 
   # リンクに装飾をつけた文字列の配列を返す。だいたい一文字づつに分かれてる。
   def styled_main_text
-    splited = message.to_show.gsub(ESCAPE_KEYS, &ESCAPE_KV)
-      links.reverse_each{ |l|
-        splited[l[:range]] = '<span underline="single" underline_color="#000000">'+"#{Pango.escape(l[:face])}</span>" }
+    splited = message.to_show.dup
+    terminate = splited.size
+    message.links.to_a.reverse_each{ |l|
+      escape_range = l[:range].last ... terminate
+      splited[escape_range] = splited[escape_range].gsub(ESCAPE_KEYS, &ESCAPE_KV)
+      splited[l[:range]] = '<span underline="single" underline_color="#000000">'+"#{Pango.escape(l[:face])}</span>"
+      terminate = l[:range].first
+    }
+    splited[0..terminate] = splited[0..terminate].gsub(ESCAPE_KEYS, &ESCAPE_KV) if terminate != 0
     splited end
-
-  # [[MatchData, 開始位置と終了位置のRangeオブジェクト(文字数), Regexp], ...] の配列を返す
-  def links
-    return message.links.to_a
-    result = Set.new
-    Gtk::TimeLine.linkrules.keys.each{ |regexp|
-      main_text.each_matches(regexp){ |match, pos|
-        if not result.any?{ |this| this[1].include?(pos) }
-          pos = [escaped_main_text.size, pos].min
-          result << [match, Range.new(pos, pos + match.to_s.size, true), regexp] end } }
-    result.sort_by{ |r| r[1].first }.freeze end
 
 end
