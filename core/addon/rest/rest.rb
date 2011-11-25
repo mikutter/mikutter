3c3482d6801774038aeb1d9a4d483d5035a4fa27
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2011-11-25 12:43:23 +0000

    REST APIが失敗した時のことを考慮していなかった refs #381
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@599 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/addon/rest/rest.rb
+++ core/addon/rest/rest.rb
@@ -10,7 +10,7 @@ Plugin.create :rest do
       if counter >= UserConfig[interval]
         counter = 0
         service.call_api(api, count: UserConfig[count]){ |messages|
-          success.call(service, messages) } end } end
+          success.call(service, messages) if messages and not messages.empty? } end } end
 
   @crawlers = [lambda{ |service| Plugin.call(:period, service) }]
   @crawlers << define_periodical_executer(:friends_timeline, :retrieve_interval_friendtl, :retrieve_count_friendtl) do |service, messages|
