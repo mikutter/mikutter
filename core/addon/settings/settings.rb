2339458e29250700654b03d434d338d4d078bb25
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2011-12-05 17:13:29 +0000

    設定のタブがいくつか表示されていない環境があった closes #386
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@604 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/addon/settings/settings.rb
+++ core/addon/settings/settings.rb
@@ -4,25 +4,45 @@ require File.expand_path File.join(File.dirname(__FILE__), 'builder')
 require File.expand_path File.join(File.dirname(__FILE__), 'basic_settings')
 
 Plugin.create(:settings) do
-  book = Gtk::Notebook.new.set_tab_pos(Gtk::POS_TOP).set_scrollable(true)
-  onboot do |service|
-    Plugin.call(:mui_tab_regist, book, 'Settings', MUI::Skin.get("settings.png")) end
 
-  on_setting_tab_regist do |box, label|
-    box = box.call if box.respond_to?(:call)
+  def book
+    @book ||= Gtk::Notebook.new.set_tab_pos(Gtk::POS_TOP).set_scrollable(true) end
+
+  def book_labels
+    book.children.map{ |child|
+      book.get_menu_label(child).text } end
+
+  def settings_add(title, box)
     container = Gtk::ScrolledWindow.new()
     container.set_policy(Gtk::POLICY_NEVER, Gtk::POLICY_AUTOMATIC)
     container.add_with_viewport(box)
-    book.append_page(container, Gtk::Label.new(label))
+
+    idx = where_should_insert_it(title, book_labels, UserConfig[:tab_order_in_settings] || [])
+    book.insert_page_menu(idx, container, Gtk::Label.new(title), Gtk::Label.new(title))
+
+    container.show_all if book.realized? and book.mapped? end
+
+  onboot do |service|
+    Plugin.call(:mui_tab_regist, book, 'Settings', MUI::Skin.get("settings.png"))
     book.show_all end
+
+  on_setting_tab_regist do |box, title|
+    unless book_labels.include? title
+      box = box.call if box.respond_to?(:call)
+      settings_add(title, box) end end
 
   on_settings do |title, definition|
-    box = Plugin::Setting.new
-    box.instance_eval(&definition)
-    container = Gtk::ScrolledWindow.new()
-    container.set_policy(Gtk::POLICY_NEVER, Gtk::POLICY_AUTOMATIC)
-    container.add_with_viewport(box)
-    book.append_page(container, Gtk::Label.new(title))
-    book.show_all end
+    unless book_labels.include? title
+      box = Plugin::Setting.new
+      box.instance_eval(&definition)
+      settings_add(title, box) end end
+
+  defined = Plugin.filtering(:defined_settings, []).first
+  if defined
+    defined.each{ |pair|
+      title, definition = pair
+      box = Plugin::Setting.new
+      box.instance_eval(&definition)
+      settings_add(title, box) } end
 
 end
