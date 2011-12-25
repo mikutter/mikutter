993ee60e40bb3e663beba49c95398829da6d5ee1
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2011-12-25 17:20:10 +0000

    Pixbufのキャッシュを実装 refs #394
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@621 03aab468-d3d2-4883-8b12-f661bbf03fa8

---
+++ core/mui/gtk_web_image_loader_image_cache_pixbuf.rb
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+miquire :mui, 'web_image_loader_image_cache', 'web_image_loader'
+
+module Gdk::WebImageLoader
+  module ImageCache
+    module Pixbuf
+      extend Pixbuf
+
+      # URLに対する画像のPixbufが残っている場合、それを返す
+      # ==== Args
+      # [url] 画像のURL
+      # [rect] 画像の寸法(Gdk::Rectangle)
+      # ==== Return
+      # キャッシュがあれば、画像の生データ(String)、見つからなければnil
+      def load(url, rect)
+        Gdk::WebImageLoader::ImageCache.synchronize(url) { # 今丁度別のスレッドで取ってきてる最中かもしれないから必要
+          if(defined?(storage[url][rect.width][rect.height]))
+            storage[url][rect.width][rect.height] end } end
+
+      # _url_ のリクエストの結果が _raw_ であるということを登録する
+      # _raw_ が偽の場合は何もしない（キャッシュされない）
+      # ==== Args
+      # [url] 画像のurl
+      # [rect] 画像の寸法(Gdk::Rectangle)
+      # [pixbuf] Pixbufのデータ
+      # ==== Return
+      # _raw_ の値
+      def save(url, rect, pixbuf)
+        return pixbuf if not pixbuf
+        Gdk::WebImageLoader::ImageCache.synchronize(url) {
+          storage[url] ||= {}
+          storage[url][rect.width] ||= {}
+          storage[url][rect.width][rect.height] = pixbuf.freeze }
+        pixbuf end
+
+      def clear
+        @storage = nil end
+
+      private
+
+      def storage
+        @storage ||= TimeLimitedStorage.new(String, Hash)
+      end
+
+    end
+  end
+end
