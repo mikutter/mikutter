d7568359f51fd68ec5344dd9ab2e83cf94e4e7a4
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2012-04-27 16:21:12 +0000

    streamingプラグインの書き直し refs #458
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@753 03aab468-d3d2-4883-8b12-f661bbf03fa8

---
+++ core/addon/streaming/streamer.rb
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+require 'thread'
+
+module ::Plugin::Streaming
+  class Streamer
+    attr_reader :thread, :service
+
+    # イベントを登録する
+    # ==== Args
+    # [name] イベント名
+    # [many] オブジェクトをまとめて配列で受け取るかどうか
+    # [&proc] イベントを受け取るオブジェクト。
+    def self.defevent(name, many=false, &proc)
+      speed_key = "#{name}_queue_delay".to_sym
+      if many
+        define_method("event_#{name}"){ |json|
+          @queue[name] ||= TimeLimitedQueue.new(HYDE, everytime{ (UserConfig[speed_key] || 100).to_f / 1000 }, &proc)
+          @queue[name].push json }
+      else
+        define_method("_event_#{name}", &proc)
+        define_method("event_#{name}"){ |json|
+          _queue = Queue.new
+          @queue[name] ||= Thread.new{
+            loop{
+              sleep((UserConfig[speed_key] || 100).to_f / 1000)
+              __send__("_event_#{name}", _queue.pop) } }
+          _queue.push json } end end
+
+    # ==== Args
+    # [service] 接続するService
+    def initialize(service)
+      @service = service
+      @thread = Thread.new(&method(:mainloop))
+      @queue = {} end
+
+    def mainloop
+      notice service.streaming{ |q|
+        if q and not q.empty?
+          parsed = JSON.parse(q) rescue nil
+          event_factory parsed if parsed end }
+    rescue => e
+      into_debug_mode e, binding
+      notice e
+      raise e end
+
+    # UserStreamを終了する
+    def kill
+      @thread.kill
+      @queue.each{ |event, queue|
+        if queue.is_a? Thread
+          queue.kill
+        else
+          queue.thread.kill end } end
+
+    private
+
+    # UserStreamで流れてきた情報を処理する
+    # ==== Args
+    # [parsed] パースされたJSONオブジェクト
+    def event_factory(json)
+      json.freeze
+      case
+      when json['friends']
+      when respond_to?("event_#{json['event']}")
+        __send__(:"event_#{json['event']}", json)
+      when json['direct_message']
+        event_direct_message(json['direct_message'])
+      when json['delete']
+        if Mopt.debug
+          Plugin.activity :system, YAML.dump(json) end
+      when !json.has_key?('event')
+        event_update(json)
+      when Mopt.debug
+        Plugin.activity :system, YAML.dump(json)
+      else
+        p json end end
+
+    defevent(:update, true) do |data|
+      events = {update: [], mention: [], mypost: []}
+      data.each { |json|
+        msg = MikuTwitter::ApiCallSupport::Request::Parser.message(json.symbolize)
+        events[:update] << msg
+        events[:mention] << msg if msg.to_me?
+        events[:mypost] << msg if msg.from_me? }
+      events.each{ |event_name, data|
+        Plugin.call(event_name, @service, data) } end
+
+    defevent(:direct_message, true) do |data|
+      Plugin.call(:direct_messages, @service, data.map{ |datum| MikuTwitter::ApiCallSupport::Request::Parser.direct_message(datum.symbolize) }) end
+
+    defevent(:favorite) do |json|
+      by = MikuTwitter::ApiCallSupport::Request::Parser.user(json['source'].symbolize)
+      to = MikuTwitter::ApiCallSupport::Request::Parser.message(json['target_object'].symbolize)
+      if(to.respond_to?(:add_favorited_by))
+        to.add_favorited_by(by, Time.parse(json['created_at'])) end end
+
+    defevent(:unfavorite) do |json|
+      by = MikuTwitter::ApiCallSupport::Request::Parser.user(json['source'].symbolize)
+      to = MikuTwitter::ApiCallSupport::Request::Parser.message(json['target_object'].symbolize)
+      if(to.respond_to?(:remove_favorited_by))
+        to.remove_favorited_by(by) end end
+
+    defevent(:follow) do |json|
+      source = MikuTwitter::ApiCallSupport::Request::Parser.user(json['source'].symbolize)
+      target = MikuTwitter::ApiCallSupport::Request::Parser.user(json['target'].symbolize)
+      if(target.is_me?)
+        Plugin.call(:followers_created, @service, [source])
+      elsif(source.is_me?)
+        Plugin.call(:followings_created, @service, [target]) end end
+
+    defevent(:list_member_added) do |json|
+      target_user = MikuTwitter::ApiCallSupport::Request::Parser.user(json['target'].symbolize) # リストに追加されたユーザ
+      list = MikuTwitter::ApiCallSupport::Request::Parser.list(json['target_object'].symbolize) # リスト
+      source_user = MikuTwitter::ApiCallSupport::Request::Parser.user(json['source'].symbolize) # 追加したユーザ
+      list.add_member(target_user)
+      Plugin.call(:list_member_added, @service, target_user, list, source_user) end
+
+    defevent(:list_member_removed) do |json|
+      target_user = MikuTwitter::ApiCallSupport::Request::Parser.user(json['target'].symbolize) # リストに追加されたユーザ
+      list = MikuTwitter::ApiCallSupport::Request::Parser.list(json['target_object'].symbolize) # リスト
+      source_user = MikuTwitter::ApiCallSupport::Request::Parser.user(json['source'].symbolize) # 追加したユーザ
+      list.remove_member(target_user)
+      Plugin.call(:list_member_removed, @service, target_user, list, source_user) end
+
+  end
+end
