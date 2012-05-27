af0f35d11d0742367e39776fee63347c2b806808
Toshiaki Asai <qtoship@gmail.com>
2012-05-27 22:17:31 +0900

    GUIプラグインで提供されるDSLで定義されたUIを実際にレンダリングするgtkプラグインを作成。最低限のプラグインを移植

--- core/plugin/gui/tab.rb
+++ core/plugin/gui/tab.rb
@@ -1,18 +1,47 @@
 # -*- coding: utf-8 -*-
 # タブのインターフェイスを提供するクラス
 
+require File.expand_path File.join(File.dirname(__FILE__), 'pane')
 require File.expand_path File.join(File.dirname(__FILE__), 'cuscadable')
 require File.expand_path File.join(File.dirname(__FILE__), 'hierarchy_child')
+require File.expand_path File.join(File.dirname(__FILE__), 'hierarchy_parent')
 
 class Plugin::GUI::Tab
 
   include Plugin::GUI::Cuscadable
   include Plugin::GUI::HierarchyChild
+  include Plugin::GUI::HierarchyParent
 
   # instanceから呼ばれる。勝手に作成しないこと
   def initialize(slug, name)
-    Plugin::GUI::Pane.add_default << self
+    super
+    position = Plugin::GUI.get_tab_order(slug)
+    if position
+      window_slug, pane_slug, order = position
+      Plugin::GUI::Pane.instance(pane_slug) << self
+    else
+      Plugin::GUI::Pane.add_default << self
+    end
     Plugin.call(:tab_created, self)
+  end
+
+  alias __set_parent_tab__ set_parent
+  def set_parent(pane)
+    Plugin.call(:gui_tab_join_pane, self, pane)
+    __set_parent_tab__(pane)
+  end
+
+  # タイムラインを作成してこの中に入れる
+  # ==== Args
+  # [slug] タイムラインスラッグ
+  # [&proc] 処理
+  # ==== Return
+  # self
+  def timeline(slug, &proc)
+    timeline = Plugin::GUI::Timeline.instance(slug)
+    self << timeline
+    timeline.instance_eval &proc if proc
+    timeline
   end
 
 end
---
+++ core/plugin/gui/timeline.rb
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+require File.expand_path File.join(File.dirname(__FILE__), 'pane')
+require File.expand_path File.join(File.dirname(__FILE__), 'cuscadable')
+require File.expand_path File.join(File.dirname(__FILE__), 'hierarchy_child')
+require File.expand_path File.join(File.dirname(__FILE__), 'tab')
+
+class Plugin::GUI::Timeline
+
+  include Plugin::GUI::Cuscadable
+  include Plugin::GUI::HierarchyChild
+
+  def initialize(slug, name)
+    super
+    Plugin.call(:timeline_created, self)
+  end
+
+  alias __set_parent_timeline__ set_parent
+  def set_parent(tab)
+    Plugin.call(:gui_timeline_join_tab, self, tab)
+    __set_parent_timeline__(tab)
+  end
+
+  def <<(messages)
+    Plugin.call(:gui_timeline_add_messages, self, messages)
+  end
+
+end
