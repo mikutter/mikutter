b0bb6827aeefe2dcd5bb70ddd6227e3b68b84741
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2012-08-21 17:16:44 +0000

    ステータスバーが更新されない問題 refs #486
    git-svn-id: svn://toshia.dip.jp/mikutter/branches/0.2@830 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/plugin/gtk/mikutter_window.rb
+++ core/plugin/gtk/mikutter_window.rb
@@ -4,14 +4,14 @@ require "gtk2"
 
 class Gtk::MikutterWindow < Gtk::Window
 
-  attr_reader :panes
+  attr_reader :panes, :statusbar
 
   def initialize(*args)
     super
     @container = Gtk::VBox.new(false, 0)
     @panes = Gtk::HBox.new(true, 0)
     @postboxes = Gtk::VBox.new(false, 0)
-    add(@container.closeup(@postboxes).pack_start(@panes).closeup(statusbar))
+    add(@container.closeup(@postboxes).pack_start(@panes).closeup(create_statusbar))
   end
 
   def add_postbox(i_postbox)
@@ -20,18 +20,18 @@ class Gtk::MikutterWindow < Gtk::Window
     set_focus(postbox.post)
     postbox.show_all end
 
+  private
+
   # ステータスバーを返す
   # ==== Return
   # Gtk::Statusbar
-  def statusbar
+  def create_statusbar
     statusbar = Gtk::Statusbar.new
+    notice "statusbar: context id: #{statusbar.get_context_id("system")}"
     statusbar.push(statusbar.get_context_id("system"), "Twitterに新しい視野を、mikutter。")
     status_button = Gtk::Button.new.add(Gtk::WebIcon.new(MUI::Skin.get("settings.png"), 16, 16))
     status_button.relief = Gtk::RELIEF_NONE
     status_button.ssc(:clicked) {
       Plugin.call(:gui_setting) }
-    statusbar.closeup(status_button) end
-  memoize :statusbar
-
-
+    @statusbar = statusbar.closeup(status_button) end
 end
