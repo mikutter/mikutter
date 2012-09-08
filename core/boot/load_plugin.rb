0003d3638c5f544c708caff3b729c31e9198a120
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2012-09-08 08:27:29 +0000

    addonディレクトリを削除
    git-svn-id: svn://toshia.dip.jp/mikutter/branches/0.2@872 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/boot/load_plugin.rb
+++ core/boot/load_plugin.rb
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # プラグインを全てロードする
 miquire :core, 'plugin'
-miquire :addon, 'addon'
 
 Miquire::Plugin.loadpath << 'plugin' << '../plugin' << '~/.mikutter/plugin'
 Miquire::Plugin.each{ |path|
