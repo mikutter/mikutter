182c429b68ce83a22b22bbb9da847b8c3f6805a0
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2012-09-14 16:45:46 +0000

    プロフィールタブを別のペインに移動した時に閉じれなくなる問題
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@931 03aab468-d3d2-4883-8b12-f661bbf03fa8

---
+++ core/plugin/gtk/tab_container.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+require "gtk2"
+
+class Gtk::TabContainer < Gtk::VBox
+  attr_reader :i_tab
+
+  def initialize(tab)
+    type_strict tab => Plugin::GUI::TabLike
+    @i_tab = tab
+    super(false, 0)
+  end
+end
