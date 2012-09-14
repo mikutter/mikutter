ed4144a9552ced0747470549be41bbaed75d2685
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2012-09-14 17:10:24 +0000

    リストの追加・削除・リスト情報の編集機能を追加
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@968 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/mui/gtk_listlist.rb
+++ core/mui/gtk_listlist.rb
@@ -5,47 +5,13 @@
 miquire :mui, 'extension'
 
 class Gtk::ListList < Gtk::CRUD
-  # attr_reader :model, :view
-
-  # # proc(iter) ... check box clicked callback
-  # #            ... iter=[bool toggled, String label, Object optional]
-  # def initialize(&proc)
-  #   super
-  #   @model = Gtk::ListStore.new(TrueClass, String, Object)
-  #   @view = Gtk::TreeView.new(@model)
-  #   add_with_viewport(@view).show_all
-  #   toggled = Gtk::CellRendererToggle.new
-  #   toggled.signal_connect('toggled'){ |toggled, path|
-  #     iter = @model.get_iter(path)
-  #     proc.call(iter) }
-  #   col = Gtk::TreeViewColumn.new('表示', toggled, :active => 0)
-  #   col.resizable = false
-  #   @view.append_column(col)
-
-  #   col = Gtk::TreeViewColumn.new('リスト名', Gtk::CellRendererText.new, :text => 1)
-  #   col.resizable = true
-  #   @view.append_column(col)
-  #   @view.set_enable_search(true).set_search_column(1).set_search_equal_func{ |model, columnm, key, iter|
-  #     not iter[columnm].include?(key) }
-  # end
-
-  def initialize(*args)
-    @updated_hook = []
-    super
-  end
 
   def column_schemer
     [{:kind => :active, :widget => :boolean, :type => TrueClass, :label => '表示'},
-     {:kind => :text, :widget => :input, :type => String, :label => 'リスト名'},
+     {:kind => :text, :type => String, :label => 'リスト名'},
      {:type => Object},
     ].freeze
   end
-
-  def updated
-    @updated_hook << Proc.new end
-
-  def on_updated(iter)
-    @updated_hook.each{ |h| h.call(iter) } end
 
   # 「自分」のアカウントが関係するTwitterリストでこのリストビューを埋める。
   # ==== Args
