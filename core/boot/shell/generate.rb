33817eafa58f18788b3f50a64b7bf5201bf42086
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2012-10-02 06:50:04 +0000

    プラグインスケルトンとspecファイルを自動生成するコマンドラインオプションを追加
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@1040 03aab468-d3d2-4883-8b12-f661bbf03fa8

---
+++ core/boot/shell/generate.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# プラグイン自動生成
+
+require "fileutils"
+
+slug = ARGV[1]
+
+unless slug
+  puts "plugin_slug not specified."
+  puts "usage: mikutter.rb #{ARGV[0]} plugin_slug"
+  exit
+end
+
+plugin_path = File.expand_path(CHIConfig::CONFROOT + "plugin/" + slug)
+FileUtils.mkdir_p(plugin_path)
+puts "directory generated: #{plugin_path}"
+File.open("#{plugin_path}/#{slug}.rb", "w"){ |io|
+  io.write <<"EOM";
+# -*- coding: utf-8 -*-
+
+Plugin.create(:#{slug}) do
+
+end
+EOM
+}
+puts "file generated: #{plugin_path}/#{slug}.rb"
