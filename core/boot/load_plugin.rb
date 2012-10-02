64838afa2e2ac2fc040278f0eee212ffd249b089
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2012-10-02 06:49:01 +0000

    specファイルがないプラグインのロードを最後に回すようにした
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@1039 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/boot/load_plugin.rb
+++ core/boot/load_plugin.rb
@@ -3,14 +3,15 @@
 miquire :core, 'plugin'
 
 Miquire::Plugin.loadpath << 'plugin' << '../plugin' << '~/.mikutter/plugin'
+spec_unsupported = []
 Miquire::Plugin.each{ |path|
   spec_filename = File.join(File.dirname(path), "spec")
   if FileTest.exist? spec_filename
     spec = YAML.load_file(spec_filename).symbolize
     Plugin.load_file(path, spec)
   else
-    Plugin.load_file(path,
-                     name: File.basename(path),
-                     slug: File.basename(path).to_sym)
-  end
-}
+    spec_unsupported << [ path,
+                          { name: File.basename(path),
+                            slug: File.basename(path).to_sym }] end }
+spec_unsupported.each{ |args|
+  Plugin.load_file(*args) }
