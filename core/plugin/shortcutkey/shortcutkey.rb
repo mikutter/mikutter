80233d9eeb5445f657ee8b792ed56b2ba203a6ea
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2012-11-17 08:29:38 +0000

    Gtk::CRUD に、新規作成、編集、削除のボタンを表示する機能を付けた closes #527
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@1057 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/plugin/shortcutkey/shortcutkey.rb
+++ core/plugin/shortcutkey/shortcutkey.rb
@@ -88,7 +88,8 @@ Plugin.create :shortcutkey do
     [key, widget, executed] end
 
   settings "ショートカットキー" do
-    pack_start(ShortcutKeyListView.new)
+    listview = ShortcutKeyListView.new
+    pack_start(Gtk::HBox.new(false, 4).add(listview).closeup(listview.buttons(Gtk::VBox)))
   end
 
 end
