e868c204a5aa119901db5d0fd3853b6c5a6d5183
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2012-12-09 12:56:29 +0000

    コミット忘れ
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@1077 03aab468-d3d2-4883-8b12-f661bbf03fa8

---
+++ core/plugin/gtk/tab_toolbar.rb
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+class Gtk::TabToolbar < Gtk::HBox
+  def initialize(imaginally, *args)
+    type_strict imaginally => Plugin::GUI::TabToolbar
+    @imaginally = imaginally
+    super(*args)
+    initialize_event
+  end
+
+  private
+
+  # イベントハンドラの登録
+  def initialize_event
+    ssc(:expose_event){
+      notice "toolbar exposed"
+    }
+    event = Plugin::GUI::Event.new(:tab_toolbar, @imaginally.parent, [])
+    Thread.new{
+      Plugin.filtering(:command, {}).first.values.select{ |command|
+        command[:icon] and command[:role] == :tab and command[:condition] === event }
+    }.next{ |commands|
+      notice "commands are"
+      notice commands
+      commands.each{ |command|
+        face = command[:show_face] || command[:name] || command[:slug].to_s
+        name = if defined? face.call then lambda{ |x| face.call(event) } else face end
+        toolitem = Gtk::Button.new
+        toolitem.add(Gtk::WebIcon.new(command[:icon], 16, 16))
+        toolitem.tooltip(name)
+        toolitem.ssc(:clicked){
+          command[:exec].call(event) }
+        closeup(toolitem) }
+      notice commands
+      show_all if not commands.empty?
+    }.trap{ |e|
+      error "error on command toolbar:"
+      error e
+    }.terminate("コマンドエラー")
+  end
+end
