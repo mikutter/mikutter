957a9f41fd01fce7d13263cdee402916b8402b5e
toshi <toshi@03aab468-d3d2-4883-8b12-f661bbf03fa8>
2012-12-16 15:59:38 +0000

    リツイートの表示に、ユーザの名前を表示するようにした
    git-svn-id: svn://toshia.dip.jp/mikutter/trunk@1098 03aab468-d3d2-4883-8b12-f661bbf03fa8

--- core/mui/cairo_sub_parts_voter.rb
+++ core/mui/cairo_sub_parts_voter.rb
@@ -5,13 +5,14 @@ miquire :mui, 'sub_parts_helper'
 require 'gtk2'
 require 'cairo'
 
-class Gdk::SubPartsVoter < Gdk::SubParts
+class ::Gdk::SubPartsVoter < Gdk::SubParts
 
   attr_reader :votes, :icon_width, :icon_height, :margin
 
   def initialize(*args)
     super
     @icon_width, @icon_height, @margin, @votes, @user_icon = 24, 24, 2, get_default_votes.to_a, Hash.new
+    @avatar_rect = []
     @icon_ofst = 0
     if not(helper.visible? or @votes.empty?)
       sid = helper.ssc(:expose_event){
@@ -27,7 +28,8 @@ class Gdk::SubPartsVoter < Gdk::SubParts
         case e.button
         when 1
           if(x >= @icon_ofst)
-            user = @votes[(x - @icon_ofst) / icon_width]
+            index = @avatar_rect.bsearch_first {|range| range.include?(x) ? 0 : range.first <=> x}
+            user = get_user_by_point(x)
             if user
               Plugin.call(:show_profile, Service.primary, user) end end end end
       false }
@@ -42,7 +44,7 @@ class Gdk::SubPartsVoter < Gdk::SubParts
           ofsty += part.height }
         if ofsty <= y and (ofsty + height) >= y
           if(x >= @icon_ofst)
-            user = @votes[(x - @icon_ofst) / icon_width]
+            user = get_user_by_point(x)
             last_motion_user = user
             if user
               tipset = user.idname end end end
@@ -60,22 +62,20 @@ class Gdk::SubPartsVoter < Gdk::SubParts
     }
   end
 
+  def get_user_by_point(x)
+    if(x >= @icon_ofst)
+      index = @avatar_rect.bsearch_first {|range| range.include?(x) ? 0 : range.first <=> x}
+      if index
+        @votes[index] end end end
+
   def render(context)
     if(not @votes.empty?)
       if helper.visible?
         context.save{
           context.translate(@margin, 0)
-          context.set_source_rgb(*(UserConfig[:mumble_basic_color] || [0,0,0]).map{ |c| c.to_f / 65536 })
-          plc = pl_count(context)
-          context.save{
-            context.translate(0, (icon_width/2) - (plc.size[1] / Pango::SCALE / 2))
-            context.show_pango_layout(plc) }
-          xpos = @icon_ofst = plc.size[0] / Pango::SCALE
-          context.translate(@icon_ofst, 0)
-          votes.each{ |user|
-            render_icon(context, user)
-            xpos += icon_width
-            break if width <= xpos } } end end
+          put_title_icon(context)
+          put_counter(context)
+          put_voter(context) } end end
     @last_height = height end
 
   def height
@@ -110,10 +110,40 @@ class Gdk::SubPartsVoter < Gdk::SubParts
   def name
     raise end
 
-  def label
+  def title_icon
     raise end
 
   private
+
+  def put_title_icon(context)
+    context.save{
+      context.set_source_pixbuf(title_icon)
+      context.paint }
+  end
+
+  def put_counter(context)
+    plc = pl_count(context)
+    context.set_source_rgb(*(UserConfig[:mumble_basic_color] || [0,0,0]).map{ |c| c.to_f / 65536 })
+    context.save{
+      context.translate(icon_width + margin, (icon_width/2) - (plc.size[1] / Pango::SCALE / 2))
+      context.show_pango_layout(plc) }
+    @icon_ofst = ((plc.size[0] / Pango::SCALE + icon_width + margin*2) / icon_width).ceil * icon_width
+  end
+
+  def put_voter(context)
+    context.translate(@icon_ofst, 0)
+    xpos = @icon_ofst
+    @avatar_rect = []
+    votes.each{ |user|
+      left = xpos
+      xpos += render_user(context, user)
+      @avatar_rect << (left...xpos)
+      break if width <= xpos } end
+
+  def render_user(context, user)
+    render_icon(context, user)
+    icon_width
+  end
 
   def render_icon(context, user)
     context.set_source_pixbuf(user_icon(user))
@@ -130,7 +160,7 @@ class Gdk::SubPartsVoter < Gdk::SubParts
     layout = context.create_pango_layout
     layout.wrap = Pango::WRAP_CHAR
     layout.font_description = Pango::FontDescription.new(UserConfig[:mumble_basic_font])
-    layout.text = "#{votes.size} #{label}"
+    layout.text = "#{votes.size}"
     layout
   end
 
