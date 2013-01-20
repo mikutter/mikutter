3e1cfb39cf4ffd57d4dded80ced0fc90ec1fe43e
Toshiaki Asai <qtoship@gmail.com>
2013-01-20 21:55:13 +0900

    TL再生成後、新しいタイムラインのフォーカスイベントにリスナを登録していなかった

--- core/plugin/gtk/gtk.rb
+++ core/plugin/gtk/gtk.rb
@@ -163,24 +163,23 @@ Plugin.create :gtk do
     notice "create timeline #{i_timeline.slug.inspect}"
     timeline = ::Gtk::TimeLine.new(i_timeline)
     @slug_dictionary.add(i_timeline, timeline)
-    focus_in_event = timeline_focus_in_event(i_timeline)
-    timeline.tl.ssc(:focus_in_event, &focus_in_event)
-    timeline_hook_keypress(timeline, &timeline_key_press_event(i_timeline))
+    handler = {
+      key_press_event: timeline_key_press_event(i_timeline),
+      focus_in_event: timeline_focus_in_event(i_timeline),
+      destroy: lambda{ |this| timeline_hook_events(timeline, handler) } }
+    timeline_hook_events(timeline, handler)
     timeline.ssc(:destroy){
       i_timeline.destroy
       false }
     timeline.show_all
   end
 
-  # Gtk::TimeLine::InnerTL にたいして、keypressイベントのリスナを登録する。
-  # InnerTLは不定期に削除されて新しいものと入れ替わるので、destroyイベントが発生する度に
-  # 新しいタイムラインにイベントリスナを登録し直す
+  # Gtk::TimeLine::InnerTL にたいして、イベントのリスナを登録する。
   # ==== Args
   # [gtk_timeline] 対象となる Gtk::TimeLine のインスタンス
-  def timeline_hook_keypress(gtk_timeline, &key_press_event)
-    gtk_timeline.tl.ssc(:key_press_event, gtk_timeline, &key_press_event)
-    gtk_timeline.tl.ssc(:destroy, gtk_timeline){
-      timeline_hook_keypress(gtk_timeline, &key_press_event) } end
+  def timeline_hook_events(gtk_timeline, handler)
+    handler.each{ |event_name, callback|
+      gtk_timeline.tl.ssc(event_name, gtk_timeline, &callback) } end
 
   # Timelineウィジェットのfocus_in_eventのコールバックを返す
   # ==== Args
