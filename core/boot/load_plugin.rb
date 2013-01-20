c3d26e488348a6702a48cf46a4fe9dc51b903be1
Toshiaki Asai <qtoship@gmail.com>
2013-01-20 12:33:10 +0900

    ロードするプラグインのスラッグを指定できるようにした refs #560

--- core/boot/load_plugin.rb
+++ core/boot/load_plugin.rb
@@ -8,10 +8,11 @@ Miquire::Plugin.each{ |path|
   spec_filename = File.join(File.dirname(path), "spec")
   if FileTest.exist? spec_filename
     spec = YAML.load_file(spec_filename).symbolize
-    Plugin.load_file(path, spec)
+    Plugin.load_file(path, spec) if Mopt.plugin.empty? or Mopt.plugin.include?(spec[:slug].to_s)
   else
     spec_unsupported << [ path,
                           { name: File.basename(path),
                             slug: File.basename(path).to_sym }] end }
 spec_unsupported.each{ |args|
-  Plugin.load_file(*args) }
+  Plugin.load_file(*args) if Mopt.plugin.empty? or Mopt.plugin.include?(args[1][:slug])
+}
