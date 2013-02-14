90b32c7a6276aa175a6cd6c5cf3fc47890e4b30b
Toshiaki Asai <qtoship@gmail.com>
2013-02-15 02:05:10 +0900

    sound pluginを追加。alsa等はそれを使用してサウンドを再生するように変更

--- core/plugin/alsa/alsa.rb
+++ core/plugin/alsa/alsa.rb
@@ -3,10 +3,7 @@
 
 Plugin.create :alsa do
 
-  on_play_sound do |filename, &stop|
-    if command_exist? "aplay"
-      SerialThread.new {
-        bg_system("aplay","-q", filename) if FileTest.exist?(filename) }
-      stop.call end end
+  defsound :alsa, "ALSA" do |filename|
+    spawn("aplay","-q", filename) if FileTest.exist?(filename) end
 
 end
---
+++ core/plugin/sound/sound.rb
@@ -0,0 +1,22 @@
+# # -*- coding: utf-8 -*-
+
+Plugin.create :sound do
+  Sound = Struct.new(:slug, :name, :play)
+
+  # サウンドDSL
+  defdslfn :defsound do |slug, name, &play|
+    filter_sound_servers do |servers|
+      servers + [Sound.new(slug, name, play)] end end
+
+  on_play_sound do |filename|
+    use_sound_server = UserConfig[:sound_server]
+    Plugin.filtering(:sound_servers, []).each{ |value|
+      if not(use_sound_server) or use_sound_server == value.slug
+        value.play.call(filename) end } end
+
+  settings "サウンド" do
+    select "サウンドの再生方法", :sound_server do
+      Plugin.filtering(:sound_servers, []).each{ |value|
+        option value.name, value.slug } end end
+
+end
