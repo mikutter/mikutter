1cc1d0e2161c766566ec9a421df1a82edd62aaee
Toshiaki Asai <toshi.alternative@gmail.com>
2013-03-27 01:03:50 +0900

    version 0.2.1.1141

--- core/config.rb
+++ core/config.rb
@@ -39,7 +39,7 @@ module CHIConfig
   # 再起動後に、前回取得したポストを取得しない
   NeverRetrieveOverlappedMumble = false
 
-  REVISION = 1137
+  REVISION = 1141
 
   # このソフトのバージョン。
   VERSION = [0,2,1, ((/Last Changed Rev\s*:\s*(\d+)/.match(`sh -c 'LC_ALL=C svn info ../'`)[1] || REVISION).to_i rescue REVISION)]
