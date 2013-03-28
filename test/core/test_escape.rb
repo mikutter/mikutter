2716378377b38db8de017c483719268126b56820
Toshiaki Asai <toshi.alternative@gmail.com>
2013-03-28 23:10:43 +0900

    不要なテストを削除

--- test/core/test_escape.rb
+++
@@ -1,5 +0,0 @@
-require 'test/unit'
-require File.expand_path(File.dirname(__FILE__) + '/../helper')
-# require File.expand_path(File.dirname(__FILE__) + '/../lib/test_unit_extensions')
-# require File.expand_path(File.dirname(__FILE__) + '/../utils')
-# miquire :lib, './escape'
