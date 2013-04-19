16f705e4d6cfa0a862909ec7ce3841d955040b7d
Toshiaki Asai <toshi.alternative@gmail.com>
2013-04-19 10:03:07 +0900

    aplayコマンドのない環境でクラッシュしていた refs #584

--- core/plugin/alsa/alsa.rb
+++ core/plugin/alsa/alsa.rb
@@ -3,7 +3,9 @@
 
 Plugin.create :alsa do
 
+  aplay_exist = command_exist?('aplay')
+
   defsound :alsa, "ALSA" do |filename|
-    bg_system("aplay","-q", filename) if FileTest.exist?(filename) end
+    bg_system("aplay","-q", filename) if FileTest.exist?(filename) and aplay_exist end
 
 end
