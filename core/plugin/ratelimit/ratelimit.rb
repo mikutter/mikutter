163147d1dd1fdd370b23f8af6bbcd1d12d7d047c
Toshiaki Asai <toshi.alternative@gmail.com>
2013-05-03 01:05:57 +0900

    規制された時、その通知をホームタイムラインに表示しないようにした

--- core/plugin/ratelimit/ratelimit.rb
+++ core/plugin/ratelimit/ratelimit.rb
@@ -2,6 +2,8 @@
 require 'set'
 
 Plugin.create :ratelimit do
+  defactivity "ratelimit", "規制通知"
+
   notificated = Set.new
   ratelimit_filter_mutex = Mutex.new
 
@@ -9,7 +11,7 @@ Plugin.create :ratelimit do
     Plugin.call(:gui_window_rewindstatus, Plugin::GUI::Window.instance(:default), "API #{ratelimit.endpoint} #{ratelimit.remain}/#{ratelimit.limit}回くらい (#{ratelimit.reset.strftime('%Y/%m/%d %H:%M:%S')}まで)", 30)
     if ratelimit.limit?
       title = "エンドポイント `#{ratelimit.endpoint}' が規制されました。#{ratelimit.reset.strftime('%Y/%m/%d %H:%M:%S')}に解除されます。"
-      activity(:system, title,
+      activity(:ratelimit, title,
                service: service,
                description: "#{title}\n#{ratelimit.endpoint} は15分に #{ratelimit.limit} 回までのアクセスが許可されています。頻発するようなら同時に使用するTwitterクライアントを減らすか、設定を見直しましょう")
     end
