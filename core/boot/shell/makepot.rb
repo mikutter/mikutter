8c67580ebc841fc4c7367747d786a6775d7d0e2d
Toshiaki Asai <toshi.alternative@gmail.com>
2013-07-06 10:36:45 +0900

    pot作成中にエラーが発生しても他のをがんばって作る

--- core/boot/shell/makepot.rb
+++ core/boot/shell/makepot.rb
@@ -9,13 +9,21 @@ mo_root = File.join(CHIConfig::CACHE, "uitranslator", "locale")
 Miquire::Plugin.loadpath << Environment::PLUGIN_PATH << File.join(File.dirname(__FILE__), "..", "..", "plugin") << File.join(Environment::CONFROOT, 'plugin')
 
 enable_plugins = ARGV[1,]
+failed_plugins = []
 
 Miquire::Plugin.each_spec do |spec|
-  if enable_plugins.empty? or enable_plugins.include? spec[:slug].to_s
+  if (enable_plugins.empty? or enable_plugins.include? spec[:slug].to_s) and
+      (defined?(spec[:depends][:plugin]) and spec[:depends][:plugin].include? "uitranslator")
     po_root = File.join spec[:path], "po"
-    GetText.update_pofiles(spec[:slug].to_s,
-                           Dir.glob("#{spec[:path]}/**/*.rb"),
-                           "#{spec[:slug]} #{spec[:version]}",
-                           po_root: po_root)
+    begin
+      GetText.update_pofiles(spec[:slug].to_s,
+                             Dir.glob("#{spec[:path]}/**/*.rb"),
+                             "#{spec[:slug]} #{spec[:version]}",
+                             po_root: po_root)
+    rescue Exception => e
+      failed_plugins << spec[:slug]
+    end
   end
 end
+
+puts "failed plugins: #{failed_plugins}" unless failed_plugins.empty?
