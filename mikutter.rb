1d3e7979c997d9d965bb3d8dbc3f3ecd4dadae10
Toshiaki Asai <toshi.alternative@gmail.com>
2014-05-14 20:41:08 +0900

    mikutter.rb のライセンス表記を修正 refs #674

--- mikutter.rb
+++ mikutter.rb
@@ -4,11 +4,9 @@
 = mikutter - the moest twitter client
 Copyright (C) 2009-2014 Toshiaki Asai
 
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+This software is released under the MIT License.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+http://opensource.org/licenses/mit-license.php
 
 =end
 mikutter_directory = File.expand_path(File.dirname(__FILE__))
