1dcfd6bf5356106c23d35710eaf4124e712f49c1
Toshiaki Asai <toshi.alternative@gmail.com>
2014-08-15 21:01:09 +0900

    listプラグインが作るタイムラインタブを削除。合わせて、リストAPIを叩く条件を、タブがあるものからデータソースが参照されているものに変更 refs #707

--- core/plugin/list/list.rb
+++ core/plugin/list/list.rb
@@ -23,8 +23,7 @@ Plugin.create :list do
     if crawl_count >= UserConfig[:retrieve_interval_list_timeline]
       crawl_count = 0
       fetch_list_of_service(Service.primary).next {
-        timelines.values.each{ |list|
-          list_modify_member(list) } }
+        Deferred.when(*using_lists.map(&method(:list_modify_member))) }.terminate
     end
   end
 
@@ -35,7 +34,7 @@ Plugin.create :list do
 
   def datasource_slug(list)
     type_strict list => UserList
-    :"#{list.user.idname}_list_#{list[:slug]}" end
+    :"#{list.user.idname}_list_#{list[:id]}" end
 
   # available_list の同期をとる。外的要因でリストが追加されたのを検出した場合。
   on_list_created do |service, lists|
@@ -44,8 +43,6 @@ Plugin.create :list do
 
   # available_list の同期をとる。外的要因でリストが削除されたのを検出した場合。
   on_list_destroy do |service, lists|
-    lists.each { |list|
-      list_set_visibility!(list, false) }
     deleted = lists.select{ |list| available_lists.include?(list) }
     set_available_lists(available_lists - deleted) if not deleted.empty? end
 
@@ -56,14 +53,15 @@ Plugin.create :list do
   # リストのタイムラインをリアルタイム更新する
   on_appear do |messages|
     messages.each{ |message|
-      timelines.each{ |slug, list|
+      using_lists.each{ |list|
         if list.related?(message)
-          Plugin.call(:extract_receive_message, datasource_slug(list), [message])
-          timeline(slug) << message end } } end
+          Plugin.call(:extract_receive_message, datasource_slug(list), [message]) end } } end
 
   on_service_registered do |service|
     if service
-      fetch_list_of_service(service, true) end end
+      fetch_list_of_service(service, true).next {
+        Deferred.when using_lists.map{ |list|
+          list_modify_member(list) } }.terminate end end
 
   on_service_destroyed do |service|
     service.lists(cache: true, user: service.user_obj).next{ |lists|
@@ -73,12 +71,7 @@ Plugin.create :list do
 
   # FILTER stream で、タイムラインを表示しているユーザをフォロー
   filter_filter_stream_follow do |users|
-    [timelines.values.inject(users){ |r, list| r.merge(list.member) }] end
-
-  def tabslug_by_list(list)
-    type_strict list => UserList
-    "list_#{list[:id]}".to_sym
-  end
+    [using_lists.inject(users){ |r, list| r.merge(list.member) }] end
 
   # 設定のGtkウィジェット
   def setting_container
@@ -86,7 +79,6 @@ Plugin.create :list do
     tab.plugin = self
     available_lists.each{ |list|
       iter = tab.model.append
-      iter[Tab::VISIBILITY] = list_visible?(list)
       iter[Tab::SLUG] = list[:full_name]
       iter[Tab::LIST] = list
       iter[Tab::NAME] = list[:name]
@@ -102,14 +94,9 @@ Plugin.create :list do
   # deferred
   def fetch_list_of_service(service, cache=:keep)
     type_strict service => Service
-    param = {:cache => cache, :user => service.user_obj}
-    service.lists(param).next{ |lists|
+    service.lists(cache: cache, user: service.user_obj).next{ |lists|
       if lists
-        set_available_lists(lists)
-        tab_reflesh{
-          lists.each{ |list|
-            tab_mark(list) } } end }.terminate
-  end
+        set_available_lists(lists) end } end
 
   # リストのメンバーを取得する
   # ==== Args
@@ -124,9 +111,8 @@ Plugin.create :list do
       list.add_member(users) if users
       Service.primary.list_statuses(:id => list[:id],
                                     :cache => cache).next{ |res|
-        if res.is_a? Array
-          slug = timelines.keys.find{ |slug| timelines[slug] == list }
-          timeline(slug) << res if slug end
+        if res.is_a?(Array) and using?(list)
+            Plugin.call(:extract_receive_message, datasource_slug(list), res) end
       }.terminate
     }.trap { |error|
       if defined?(error.httpresponse.code) && 404 == error.httpresponse.code.to_i
@@ -138,65 +124,25 @@ Plugin.create :list do
                 list_name: list[:full_name],
                 list_id: list[:id] }) end
 
-  # 表示中のタイムライン/タブのスラッグとリストオブジェクトの連想配列
-  def timelines
-    @timelines ||= {} end
-
-  # 表示設定されているリストのIDを返す
+  # 現在データソースで使用されているリストを返す
   # ==== Return
-  # 表示できるリストのIDの配列(TypedArray)
-  def visible_list_ids
-    at(:visible_lists, []).freeze end
+  # Enumerable データソースで使われているリスト
+  def using_lists
+    list_ids = Plugin.filtering(:extract_tabs_get, []).first.inject(Set.new){ |r,tab|
+      r.merge tab[:sources]
+    }.map{ |source|
+      $1.to_i if source.to_s =~ /[a-zA-Z0-9_]+_list_(\d+)/
+    }.compact
+    available_lists.select do |list|
+      list_ids.include? list[:id] end end
 
-  # リストを表示可能に設定する。すでに表示可能にセットされている場合は何もしない
+  # list が抽出タブで使われていて、更新を要求されているなら真を返す
   # ==== Args
-  # [list] 表示可能にするリスト
+  # [list] UserList 調べるリスト
   # ==== Return
-  # self
-  def list_set_visibility(list)
-    type_strict list => UserList
-    visible_lists = at(:visible_lists, [])
-    if not visible_lists.include?(list[:id])
-      store(:visible_lists, (visible_lists + [list[:id]]).uniq) end
-    self end
-
-  # リストを非表示に設定する。すでに非表示にセットされている場合は何もしない
-  # ==== Args
-  # [list] 非表示にするリスト
-  # ==== Return
-  # self
-  def list_set_hide(list)
-    type_strict list => UserList
-    visible_lists = at(:visible_lists, [])
-    if visible_lists.include?(list[:id])
-      store(:visible_lists, visible_lists - [list[:id]])
-    end
-    self end
-
-  # _list_ の表示可否状態を _visibility_ にして、実際に表示/非表示を切り替える
-  # ==== Args
-  # [list] リスト
-  # [visibility] trueなら表示、falseなら非表示
-  # ==== Return
-  # self
-  def list_set_visibility!(list, visibility)
-    type_strict list => UserList
-    if visibility
-      list_set_visibility(list)
-      tab_open(list)
-    else
-      list_set_hide(list)
-      tab_close(list) end
-    self end
-
-  # そのタブが表示する設定になっているかどうかを返す
-  # ==== Args
-  # [list] リスト
-  # ==== Return
-  # タブが表示する設定になっているなら真
-  def list_visible?(list)
-    type_strict list => UserList
-    visible_list_ids.include? list[:id] end
+  # 使われていたら真
+  def using?(list)
+    using_lists.include? list end
 
   # 自分がフォローしているリストを返す
   # ==== Return
@@ -218,86 +164,32 @@ Plugin.create :list do
     Plugin.call(:list_data, Service.primary, @available_lists) if not(created.empty? and deleted.empty?)
     @available_lists end
 
-  # このブロック中で _add_tab(list)_ を呼ばれなかったリストは、ブロックを出た時に全て削除される。
-  # また、新たにマークを付けられたタブは、タブが作成される。
-  # ==== Return
-  # ブロックの戻り値
-  def tab_reflesh
-    @tab_reflesh ||= Mutex.new
-    @tab_reflesh.synchronize do
-      @mark = Set.new
-      result = yield
-      available_lists.each{ |list|
-        if @mark.include? list
-          tab_open(list)
-        else
-          tab_close(list) end }
-      result end end
-
-  # _list_ に対応するタブにマークをつける。
-  # ==== Args
-  # [list] リスト
-  def tab_mark(list)
-    type_strict list => UserList
-    if list_visible?(list)
-      @mark << list end end
-
-  # _list_ のためのタブを開く。タブがすでに有る場合は何もしない。
-  # ==== Args
-  # [list] リスト
-  # ==== Return
-  # self
-  def tab_open(list)
-    type_strict list => UserList
-    slug = tabslug_by_list list
-    return self if timelines.has_key? slug
-    timelines[slug] = list
-    tab(slug, list[:full_name]) do
-      set_icon Skin.get("list.png")
-      timeline slug end
-    list_modify_member(list, true)
-    self end
-
-  # _list_ のためのタブを閉じる。タブがない場合は何もしない。
-  # ==== Args
-  # [list] リスト
-  # ==== Return
-  # self
-  def tab_close(list)
-    type_strict list => UserList
-    slug = timelines.keys.find{ |slug| timelines[slug] == list }
-    return self if not timelines.has_key? slug
-    if slug
-      timelines.delete(slug)
-      tab(slug).destroy end
-    self end
-
   ->(service) {
     if service
       Delayer.new{
-        fetch_list_of_service(service, true) } end }.(Service.primary)
+        fetch_list_of_service(service, true).next {
+          Deferred.when(*using_lists.map(&method(:list_modify_member)))
+        }.terminate } end }.(Service.primary)
 
   class IDs < TypedArray(Integer); end
 
   class Tab < ::Gtk::ListList
     attr_accessor :plugin
 
-    VISIBILITY = 0
-    SLUG = 1
-    LIST = 2
-    NAME = 3
-    DESCRIPTION = 4
-    PUBLICITY = 5
+    SLUG = 0
+    LIST = 1
+    NAME = 2
+    DESCRIPTION = 3
+    PUBLICITY = 4
 
     def initialize(plugin)
       type_strict plugin => Plugin
       @plugin = plugin
       super()
-      dialog_title = "リスト" end
+      self.dialog_title = "リスト" end
 
     def column_schemer
-      [{:kind => :active, :widget => :boolean, :type => TrueClass, :label => @plugin._('表示')},
-       {:kind => :text, :type => String, :label => @plugin._('リスト名')},
+      [{:kind => :text, :type => String, :label => @plugin._('リスト名')},
        {:type => UserList},
        {:type => String, :widget => :input, :label => @plugin._('リストの名前')},
        {:type => String, :widget => :input, :label => @plugin._('リスト説明')},
@@ -315,13 +207,11 @@ Plugin.create :list do
           Plugin.call(:list_created, Service.primary, UserLists.new([list]))
           if not(destroyed?)
             iter[LIST] = list
-            iter[SLUG] = list[:full_name]
-            list_set_visibility!(list, iter[VISIBILITY]) end end } end
+            iter[SLUG] = list[:full_name] end end } end
 
     def on_updated(iter)
       list = iter[LIST]
       if list
-        plugin.list_set_visibility!(list, iter[VISIBILITY])
         if list[:name] != iter[NAME] || list[:description] != iter[DESCRIPTION] || list[:mode] != iter[PUBLICITY]
           notice "list updated. #{iter[NAME]} #{iter[DESCRIPTION]} #{iter[PUBLICITY]}"
           Service.primary.update_list(id: list[:id],
