54fc4b621a4fc990710cb1fdc9675084187f4e96
Toshiaki Asai <toshi.alternative@gmail.com>
2014-08-29 09:29:51 +0900

    HierarchycalSelectboxの最適化

--- core/mui/gtk_hierarchycal_selectbox.rb
+++ core/mui/gtk_hierarchycal_selectbox.rb
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 require 'gtk2'
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'utils'))
 miquire :mui, 'selectbox'
 
 =begin rdoc
@@ -20,45 +19,26 @@ class Gtk::HierarchycalSelectBox < Gtk::SelectBox
   end
 
   def setting_values(values, selected)
-    parent_node = {}            # name => TreeIter
-    parent_node_list(values.map(&:last)).each do |name, parent|
-      iter = parent_node[name] = model.append(parent_node[parent])
-      iter[ITER_ID] = -1
-      iter[ITER_STRING] = child_name(name) end
-    values.reject{|_,name| parent_node.include? name}.each{ |pair|
-      id, string = *pair
-      iter = model.append(parent_node[parent_name(string)])
-      iter[ITER_ID] = id
-      iter[ITER_STRING] = child_name(string)
-      iter[ITER_CHECK] = (selected and @selected.include?(id)) } end
-
-  # /区切りの文字列からなる配列を受け取って、各要素の最後の/の前の値を返す
-  # /が含まれていない文字列に関しては、それを取り除く。
-  # 戻り値は、より親のノードが要素の先に存在することを保証する。
-  # ==== Args
-  # [namelist] Array 文字列の配列
-  # ==== Return
-  # Array 親の名前のリスト
-  def parent_node_list(namelist)
-    namelist.lazy.select{|_|
-      _.include? DELIMITER
-    }.map(&method(:parent_name)).sort_by{ |node|
-      [node.count(DELIMITER), namelist.index{|n| n.start_with? node}||0] }.uniq end
-
-  # /で区切られた文字列の最後を除いた要素を取得する
-  # ==== Args
-  # [path] String /区切りの文字列
-  # ==== Return
-  # String
-  def parent_name(path)
-    path.split(DELIMITER)[0..-2].join(DELIMITER) end
-
-  # /で区切られた文字列の最後の要素を取得する
-  # ==== Args
-  # [path] String /区切りの文字列
-  # ==== Return
-  # String
-  def child_name(path)
-    path.split(DELIMITER)[-1] end
+    root_nodes = []
+    values.each do |pair|
+      id, name = *pair
+      fullpath = []
+      last_node = name.split(DELIMITER).inject(nil) do |parent_node, hierarchy|
+        fullpath << hierarchy
+        if parent_node
+          node = parent_node.n_children.times.lazy.map(&parent_node.method(:nth_child)).find{|_| _[ITER_STRING] == hierarchy } || model.append(parent_node)
+        else
+          node = root_nodes.find{|_| _[ITER_STRING] == hierarchy }
+          if node.nil?
+            node = model.append(nil)
+            root_nodes << node end end
+        node[ITER_ID] = -1
+        node[ITER_STRING] = hierarchy
+        node
+      end
+      last_node[ITER_ID] = id
+      last_node[ITER_CHECK] = (selected and @selected.include?(id))
+    end
+  end
 
 end
