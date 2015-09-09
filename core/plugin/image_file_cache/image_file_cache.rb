0f028f04a9fae9cb682be81be430b634d571800e
Toshiaki Asai <toshi.alternative@gmail.com>
2015-09-09 20:09:58 +0900

    有効期限が切れた画像キャッシュを削除する

--- core/plugin/image_file_cache/image_file_cache.rb
+++ core/plugin/image_file_cache/image_file_cache.rb
@@ -8,12 +8,14 @@ end
 
 Plugin.create :image_file_cache do
 
-  Delayer.new do
-    @db = ::Moneta.build do
-      use :Transformer, key: [:md5, :spread]
-      adapter :File, dir: File.join(Environment::CACHE, 'image_file_cache')
-    end
-  end
+  @queue = Delayer.generate_class(priority: %i[none check_subdirs check_dirs],
+                                  default: :none,
+                                  expire: 0.02)
+  @cache_directory = File.join(Environment::CACHE, 'image_file_cache').freeze
+  @db = ::Moneta.build(&->(dir){ ->(this){
+                                   this.use :Transformer, key: %i[md5 spread]
+                                   this.adapter(:File, dir: dir)
+                                 } }.(@cache_directory))
 
   # appear_limit 回TLに出現したユーザはキャッシュに登録する
   # (30分ツイートしなければカウンタはリセット)
@@ -56,5 +58,47 @@ Plugin.create :image_file_cache do
       raw = Gdk::WebImageLoader.get_raw_data(image_url)
       if raw
         @db[image_url] = raw end end end
+
+  def check_subdirs(dir)
+    @queue.new(:check_subdirs) do
+      Dir.foreach(dir)
+        .map{|x| File.join(dir, x) }
+        .select{|x| FileTest.file?(x) }
+        .each{|x|
+        Reserver.new((File.atime(x) rescue File.mtime(x)) + cache_expire) do
+          notice "cache deleted #{x}"
+          File.delete(x) if FileTest.file?(x)
+          if Dir.foreach(dir).select{|y| File.file? File.join(dir, y) }.empty?
+            Dir.delete(dir) rescue nil end end
+      }
+    end
+  end
+
+  def check_dirs
+    @queue.new(:check_dirs) do
+      Dir.foreach(@cache_directory)
+        .select{|x| x =~ %r<\A[a-fA-F0-9]{2}\Z> }
+        .shuffle
+        .each{|subdir|
+        check_subdirs(File.join(@cache_directory, subdir))
+      }
+      Reserver.new(cache_expire) do
+        check_dirs end
+    end
+  end
+
+  def _loop
+    Reserver.new(60) do
+      if @queue
+        @queue.run
+        _loop  end end end
+
+  on_unload do
+    @db.close
+    @db = @queue = nil
+  end
+
+  check_dirs
+  _loop
 
 end
