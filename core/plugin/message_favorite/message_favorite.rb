85b897845859a2cfb5415ac7efe323c33db3ece0
Toshiaki Asai <toshi.alternative@gmail.com>
2016-04-17 19:03:12 +0900

    message_favorite: リアルタイムふぁぼられ

--- core/plugin/message_favorite/message_favorite.rb
+++ core/plugin/message_favorite/message_favorite.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 Plugin.create :message_favorite do
   message_fragment :favorited, "Favorite" do
+    message = retriever
     set_icon Skin.get('unfav.png')
     user_list = Gtk::UserList.new
     begin
@@ -9,5 +10,10 @@ Plugin.create :message_favorite do
       error err
     end
     nativewidget user_list
+
+    on_favorite do |service, user, to_message|
+      if to_message == message
+        user_list.add_user(Users.new([user])) end end
+
   end
 end
