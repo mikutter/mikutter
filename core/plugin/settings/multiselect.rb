567395d67241dc2f6caa0c7476f544497e73d48f
Toshiaki Asai <toshi.alternative@gmail.com>
2016-04-26 02:41:45 +0900

    settings: selectとmultiselectがイベントハンドラを定義する方法を変更
    sscを使ってウィジェット削除時にクラッシュする問題の対策
    Procが不必要な参照をできるだけ持たないように外部メソッド化

--- core/plugin/settings/multiselect.rb
+++ core/plugin/settings/multiselect.rb
@@ -30,33 +30,27 @@ class Plugin::Settings::MultiSelect < Plugin::Settings::Select
     box = Gtk::VBox.new
 
     options = @options
-    box.instance_eval{
-      options.each{ |value, face, setting|
-        if (not setting) and face.is_a? String
-          closeup check = Gtk::CheckButton.new(face)
-        elsif setting.is_a? Plugin::Settings
-          if face.is_a? String
-            container = Gtk::Table.new(2, 2)
-            check = Gtk::CheckButton.new
-            container.attach(check, 0, 1, 0, 1, Gtk::FILL, Gtk::FILL)
-            container.attach(Gtk::Label.new(face).left, 1, 2, 0, 1, Gtk::SHRINK|Gtk::FILL, Gtk::FILL)
-            container.attach(setting, 1, 2, 1, 2, Gtk::FILL|Gtk::SHRINK|Gtk::EXPAND, Gtk::FILL|Gtk::SHRINK|Gtk::EXPAND)
-            closeup container
-          else
-            container = Gtk::HBox.new
-            check = Gtk::CheckButton.new
-            closeup container.closeup(check).add(setting) end
+    options.each{ |value, face, setting|
+      if (not setting) and face.is_a? String
+        box.closeup check = Gtk::CheckButton.new(face)
+      elsif setting.is_a? Plugin::Settings
+        if face.is_a? String
+          container = Gtk::Table.new(2, 2)
+          check = Gtk::CheckButton.new
+          container.attach(check, 0, 1, 0, 1, Gtk::FILL, Gtk::FILL)
+          container.attach(Gtk::Label.new(face).left, 1, 2, 0, 1, Gtk::SHRINK|Gtk::FILL, Gtk::FILL)
+          container.attach(setting, 1, 2, 1, 2, Gtk::FILL|Gtk::SHRINK|Gtk::EXPAND, Gtk::FILL|Gtk::SHRINK|Gtk::EXPAND)
+          box.closeup container
         else
-          raise ArgumentError, "multiselect option value should be instance of String or Plugin::Settings. but #{face.class} given (#{face.inspect})"
-        end
-        check.signal_connect('toggled'){ |widget|
-          if widget.active?
-            listener.set((listener.get || []) + [value])
-          else
-            listener.set((listener.get || []) - [value]) end
-          setting.sensitive = widget.active? if setting.is_a? Gtk::Widget }
-        check.active = (listener.get || []).include? value
-        setting.sensitive = check.active? if setting.is_a? Gtk::Widget } }
+          container = Gtk::HBox.new
+          check = Gtk::CheckButton.new
+          box.closeup container.closeup(check).add(setting) end
+      else
+        raise ArgumentError, "multiselect option value should be instance of String or Plugin::Settings. but #{face.class} given (#{face.inspect})"
+      end
+      check.ssc(:toggled, &generate_toggled_listener(listener, value, setting))
+      check.active = (listener.get || []).include? value
+      setting.sensitive = check.active? if setting.is_a? Gtk::Widget }
     box end
 
   # すべてテキストなら、コンボボックスで要素を描画する
@@ -67,11 +61,24 @@ class Plugin::Settings::MultiSelect < Plugin::Settings::Select
       node, value = *pair
       check = Gtk::CheckButton.new(value)
       check.active = state.include?(node)
-      check.signal_connect('toggled'){ |widget|
-        if widget.active?
-          listener.set((listener.get || []) + [node])
-        else
-          listener.set((listener.get || []) - [node]) end }
+      check.ssc(:toggled, &generate_toggled_listener(listener, value))
       container.closeup check }
     container end
+
+  def generate_toggled_listener(listener, value, setting=nil)
+    if setting.is_a? Gtk::Widget
+      ->(widget) do
+        if widget.active?
+          listener.set((listener.get || []) + [value])
+        else
+          listener.set((listener.get || []) - [value]) end
+        setting.sensitive = widget.active?
+      false end
+    else
+      ->(widget) do
+        if widget.active?
+          listener.set((listener.get || []) + [value])
+        else
+          listener.set((listener.get || []) - [value]) end
+        false end end end
 end
