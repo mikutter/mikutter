5a56709b4be80f6d8bc4c87795dc609416b8bb72
Toshiaki Asai <toshi.alternative@gmail.com>
2016-10-22 18:55:04 +0900

    次のintentで開く refs #906

--- core/plugin/intent/model/intent_token.rb
+++ core/plugin/intent/model/intent_token.rb
@@ -5,10 +5,16 @@ module Plugin::Intent
     field.string :uri, required: true
     field.has    :model, Retriever::Model
     field.has    :intent, Plugin::Intent::Intent, required: true
+    field.has    :parent, Plugin::Intent::IntentToken
 
     # 引数の情報からIntentTokenを作成し、それを開く
     def self.open(*args)
       self.new(*args).open
+    end
+
+    def initialize(*rest)
+      super
+      self[:source] = self[:model]
     end
 
     # 設定された情報を使ってURI又はModelを開く
@@ -24,6 +30,31 @@ module Plugin::Intent
         }.terminate('なんか開けなかった')
       end
       self
+    end
+
+    def forward
+      Plugin.call(:intent_forward, self)
+    end
+
+    # _self_ から親のIntentTokenを再帰的に遡って、そのIntentTokenを引数に繰り返すEnumeratorを返す。
+    # ==== Return
+    # [Enumerator] IntentTokenを列挙する
+    def ancestors
+      Enumerator.new do |yielder|
+        cur = self
+        loop do
+          break unless cur
+          yielder << cur
+          cur = cur.parent
+        end
+      end
+    end
+
+    # ancestorsと同じようなもの。ただし、IntentToken#intentに関して繰り返す。
+    # ==== Return
+    # [Enumerator] Intentを列挙する
+    def intent_ancestors
+      ancestors.lazy.map(&:intent)
     end
   end
 end
