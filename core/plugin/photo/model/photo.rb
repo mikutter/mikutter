8d6a8556d18dfa23cab243573ba0108c242adf70
Toshiaki Asai <toshi.alternative@gmail.com>
2016-11-18 02:59:32 +0900

    Photoのインスタンスをできるだけ使いまわす refs #934

--- core/plugin/photo/model/photo.rb
+++ core/plugin/photo/model/photo.rb
@@ -7,5 +7,18 @@ module Plugin::Photo
     register :photo, name: Plugin[:photo]._('画像')
 
     field.uri :perma_link
+
+    def self.photos
+      @photos ||= TimeLimitedStorage.new(Integer, self)
+    end
+
+    def self.[](uri)
+      photos[uri.to_s.hash] ||= new(perma_link: uri)
+    end
+
+    def initialize(*params)
+      super
+      self.class.photos[uri.to_s.hash] = self
+    end
   end
 end
