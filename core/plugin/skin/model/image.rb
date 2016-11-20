47c791c7be1516cc66b0199605ea39e7c2cdbdec
Toshiaki Asai <toshi.alternative@gmail.com>
2016-11-21 08:54:56 +0900

    Skin関連のリソースをGtkに依存せず扱うためにプラグインを分けた refs #934

---
+++ core/plugin/skin/model/image.rb
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+miquire :lib, 'retriever/mixin/photo_mixin'
+
+module Plugin::Skin
+  class Image < Retriever::Model
+    include Retriever::Model::PhotoMixin
+
+    register :skin_image, name: 'skin image'
+
+    field.string :path, required: true
+
+    def self.[](path)
+      @store ||= Hash.new{|h,k| h[k] = new(path: k) } # path => Image
+      @store[path]
+    end
+
+    memoize def uri
+      URI::Generic.build(scheme: 'file'.freeze, path: path).freeze
+    end
+
+    # 引数の寸法の GdkPixbuf::Pixbuf を返す。
+    # ==== Args
+    # [width:] Pixbufの幅(px)
+    # [height:] Pixbufの高さ(px)
+    # ==== Return
+    # [GdkPixbuf::Pixbuf] メモリキャッシュやファイルシステムから画像が見つかった場合
+    def pixbuf(width:, height:)
+      result = pixbuf_cache[[width, height].hash]
+      if result
+        result.pixbuf
+      else
+        pixbuf_cache_set(GdkPixbuf::Pixbuf.new(file: uri.path, width: width, height: height), width: width, height: height)
+      end
+    end
+
+    private
+
+    def pixbuf_forget(width, height, gen)
+      unless width == height and [12, 16, 24, 32, 48, 64].include?(width)
+        Reserver.new([300, 60 * gen ** 2].max) do
+          pixbuf_cache.delete([width, height].hash)
+        end
+      end
+    end
+  end
+end
