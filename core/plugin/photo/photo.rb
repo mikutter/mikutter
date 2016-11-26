b20becb96517b9675b4c184703c62bb9ded95dca
Toshiaki Asai <toshi.alternative@gmail.com>
2016-11-27 06:16:34 +0900

    PhotoModel系のクラスを直接指定しなくても画像を取得する手段を提供 refs #934
    photo_filter フィルタを使えばなんなりとModelを取得できるようにした

--- core/plugin/photo/photo.rb
+++ core/plugin/photo/photo.rb
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 require_relative 'model/photo'
 Plugin.create :photo do
+  # 第一引数のpermalinkを指すModelを全て取得するフィルタ。
+  # このフィルタでは、画像を指すModelしか返ってこない
+  defevent :photo_filter,
+           prototype: [Object, :<<]
+
+  filter_photo_filter do |permalink, photos|
+    photos << Plugin::Photo::Photo[permalink]
+    [permalink, photos]
+  end
 end
 
