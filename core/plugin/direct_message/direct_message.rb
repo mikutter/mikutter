f7c5b7529e1915987113a57a655da82fbd284cf1
Toshiaki Asai <toshi.alternative@gmail.com>
2017-12-19 06:04:19 +0900

    Twitter Worldが一つもない場合は、Twitterのためのタブを生成しない
    home_timeline
    mentions
    directmessage
    search

--- core/plugin/direct_message/direct_message.rb
+++ core/plugin/direct_message/direct_message.rb
@@ -6,15 +6,75 @@ require_relative 'dmlistview'
 
 module Plugin::DirectMessage
   Plugin.create(:direct_message) do
-    def userlist
-      @userlist ||= UserList.new end
-
     @counter = gen_counter
-    ul = userlist
-    tab(:directmessage, _("DM")) do
-      set_icon Skin['directmessage.png']
-      expand
-      nativewidget ul
+
+    Delayer.new do
+      refresh_tab
+      interval
+    end
+
+    on_world_create do |world|
+      refresh_tab
+    end
+
+    on_world_destroy do |world|
+      refresh_tab
+    end
+
+    def refresh_tab
+      if Enumerator.new{|y| Plugin.filtering(:worlds, y) }.any?{|w| w.class.slug == :twitter }
+        present_tab
+      else
+        absent_tab
+      end
+    end
+
+    def present_tab
+      @tag ||= handler_tag do
+        userlist = UserList.new
+        tab(:directmessage, _("DM")) do
+          set_icon Skin['directmessage.png']
+          expand
+          nativewidget userlist
+        end
+
+        on_direct_messages do |_, dms|
+          dm_distribution = Hash.new {|h,k| h[k] = []}
+          dms.each do |dm|
+            model = Plugin::Twitter::DirectMessage.new_ifnecessary(dm)
+            dm_distribution[model[:user]] << model
+            dm_distribution[model[:recipient]] << model
+          end
+          dm_distribution.each do |to_user, dm_for_user|
+            Plugin::GUI::Timeline.instance(timeline_name_for(to_user)) << dm_for_user
+            Plugin.call :extract_receive_message, timeline_name_for(to_user), dm_for_user
+          end
+          Plugin.call :extract_receive_message, :direct_message, dms
+          userlist.update(dm_distribution.map{|k, v| [k, v.map{|dm| dm[:created]}.max]}.to_h)
+        end
+
+        filter_extract_datasources do |datasources|
+          datasources = {
+            direct_message: _("ダイレクトメッセージ"),
+          }.merge datasources
+          Enumerator.new{|y|
+            Plugin.filtering(:worlds, y)
+          }.lazy.select{|world|
+            world.class.slug == :twitter
+          }.map(&:user_obj).each{ |user|
+            datasources.merge!({ extract_slug_for(user) => "@#{user.idname}/" + _("ダイレクトメッセージ") })
+          }
+          [datasources]
+        end
+      end
+    end
+
+    def absent_tab
+      if @tag
+        tab(:directmessage).destroy
+        detach(@tag)
+        @tag = nil
+      end
     end
 
     user_fragment(:directmessage, _("DM")) do
@@ -25,40 +85,12 @@ module Plugin::DirectMessage
       end
     end
 
-    filter_extract_datasources do |datasources|
-      datasources = {
-        direct_message: _("ダイレクトメッセージ"),
-      }.merge datasources
-      Enumerator.new{|y|
-        Plugin.filtering(:worlds, y)
-      }.lazy.select{|world|
-        world.class.slug == :twitter
-      }.map(&:user_obj).each{ |user|
-        datasources.merge!({ extract_slug_for(user) => "@#{user.idname}/" + _("ダイレクトメッセージ") })
-      }
-      [datasources] end
-
     def extract_slug_for(user)
       "direct_message-#{user.id}".to_sym
     end
 
-    on_direct_messages do |_, dms|
-      dm_distribution = Hash.new {|h,k| h[k] = []}
-      dms.each do |dm|
-        model = Plugin::Twitter::DirectMessage.new_ifnecessary(dm)
-        dm_distribution[model[:user]] << model
-        dm_distribution[model[:recipient]] << model
-      end
-      dm_distribution.each do |to_user, dm_for_user|
-        Plugin::GUI::Timeline.instance(timeline_name_for(to_user)) << dm_for_user
-        Plugin.call :extract_receive_message, timeline_name_for(to_user), dm_for_user
-      end
-      Plugin.call :extract_receive_message, :direct_message, dms
-      ul.update(dm_distribution.map{|k, v| [k, v.map{|dm| dm[:created]}.max]}.to_h)
-    end
-
     def timeline_name_for(user)
-      :"direct_messages_from_#{user.idname}"
+       :"direct_messages_from_#{user.idname}"
     end
 
     def interval
@@ -81,8 +113,5 @@ module Plugin::DirectMessage
         error e
       }
     end
-
-    Delayer.new{ interval }
-
   end
 end
