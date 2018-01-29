4c5a96fabe0bb6623a3563db96f75efaf324e483
Yuto Tokunaga <yuntan.sub1@gmail.com>
2018-01-29 15:42:31 +0000

    install gems

--- scripts/gen_appimage.sh
+++ scripts/gen_appimage.sh
@@ -24,32 +24,24 @@ tar xf ruby-2.3.6.tar.gz
 
 echo "--> compile Ruby and install it into AppDir"
 pushd ruby-2.3.6
-./configure --prefix=/usr
+./configure "--prefix=$APP_DIR/usr"
 make -j2
-make "DESTDIR=$APP_DIR" install
+make install
 popd
 
-echo "--> patch away absolute paths"
-sed -i -e 's|/usr|././|g' "$APP_DIR/usr/bin/ruby"
-
 echo "--> install gems"
-pushd "$APP_DIR/usr"
-./bin/ruby ./bin/gem install bundler
+./$APP_DIR/usr/bin/gem install bundler
+./$APP_DIR/usr/bin/bundle install
 
-# remove doc, man, ri
-rm -rf "$APP_DIR/usr/share"
-
-########################################################################
-# Get helper functions and move to AppDir
-########################################################################
+echo "--> get helper functions"
 wget -q https://github.com/AppImage/AppImages/raw/master/functions.sh -O ./functions.sh
 . ./functions.sh
 
-# Copy desktop and icon file to AppDir for AppRun to pick them up.
-# get_apprun
-# get_desktop
-# cp "$ROOT_DIR/runtime/nvim.desktop" "$APP_DIR/"
-# cp "$ROOT_DIR/runtime/nvim.png" "$APP_DIR/"
+echo "--> patch away absolute paths"
+patch_strings_in_file "$APP_DIR/usr/bin/ruby" "$APP_DIR/usr" ""
+
+echo "--> remove doc, man, ri"
+rm -rf "$APP_DIR/usr/share"
 
 pushd "$APP_DIR"
 
@@ -74,10 +66,6 @@ delete_blacklisted
 
 popd
 
-########################################################################
-# AppDir complete. Now package it as an AppImage.
-########################################################################
-
 echo "--> enable fuse"
 sudo modprobe fuse
 sudo usermod -a -G fuse $(whoami)
@@ -88,15 +76,7 @@ echo "--> generate AppImage"
 #   - Produces: ../out/$APP-$VERSION.glibc$GLIBC_NEEDED-$ARCH.AppImage
 generate_appimage
 
-# NOTE: There is currently a bug in the `generate_appimage` function (see
-# https://github.com/probonopd/AppImages/issues/228) that causes repeated builds
-# that result in the same name to fail.
-# Moving the final executable to a different folder gets around this issue.
-
-# mv "$ROOT_DIR"/out/*.AppImage "$ROOT_DIR"/build/bin
-# Remove the (now empty) folder the AppImage was built in
-# rmdir "$ROOT_DIR"/out
-
+echo "--> generated $APP-$VERSION-$ARCH.AppImage"
 mv ../out/*.AppImage "$TRAVIS_BUILD_DIR/${APP}-${VERSION}-${ARCH}.AppImage"
 
 echo '==> finished'
