1b57b0e6e05c59296f8f9dba3e62e67fc14bd040
cobodo <cobodo@gmail.com>
2018-02-01 16:19:14 +0900

    subparts-image対応

--- entity_class.rb
+++ entity_class.rb
@@ -2,7 +2,7 @@ module Diva::Entity
   AnchorLinkEntity = RegexpEntity.filter(/<a [^>]*>[^<]*<\/a>/, generator: -> h {
     a = h[:url]
     if h[:url] =~ /<a [^>]*href="([^"]*)"[^>]*>([^<]*)<\/a>/
-      h[:open] = $1
+      h[:url] = h[:open] = $1
       h[:face] = $2
     end
     h
