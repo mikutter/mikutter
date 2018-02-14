127b28a31cca3d8d72e923b487d4d1db48a48501
cobodo <cobodo@gmail.com>
2018-02-14 23:15:28 +0900

    存在しないドメインに対してリクエストするとmikutterごと落ちる不具合を修正

--- api.rb
+++ api.rb
@@ -6,44 +6,52 @@ module Plugin::Worldon
   class API
     class << self
       def call(method, domain, path, access_token = nil, **opts)
-        url = 'https://' + domain + path
-        uri = Diva::URI.new(url)
-        case method
-        when :get
-          path = uri.path
-          if !opts.empty?
-            path += '?' + URI.encode_www_form(opts)
+        begin
+          url = 'https://' + domain + path
+          uri = Diva::URI.new(url)
+          case method
+          when :get
+            path = uri.path
+            if !opts.empty?
+              path += '?' + URI.encode_www_form(opts)
+            end
+            req = Net::HTTP::Get.new(path)
+          when :post
+            req = Net::HTTP::Post.new(uri.path)
+            req.set_form_data(opts)
           end
-          req = Net::HTTP::Get.new(path)
-        when :post
-          req = Net::HTTP::Post.new(uri.path)
-          req.set_form_data(opts)
-        end
 
-        if !access_token.nil? && access_token.length > 0
-          req["Authorization"] = "Bearer " + access_token
-        end
+          if !access_token.nil? && access_token.length > 0
+            req["Authorization"] = "Bearer " + access_token
+          end
 
-        notice "Worldon::API.call #{method.to_s} #{domain} #{req.path}"
+          notice "Worldon::API.call #{method.to_s} #{domain} #{req.path}"
 
-        http = Net::HTTP.new(uri.host, uri.port)
-        #http.set_debug_output $stderr
-        if uri.scheme == 'https'
-          http.use_ssl = true
-        end
+          http = Net::HTTP.new(uri.host, uri.port)
+          #http.set_debug_output $stderr
+          if uri.scheme == 'https'
+            http.use_ssl = true
+          end
 
-        resp = http.start do |http|
-          http.request(req)
-        end
+          resp = http.start do |http|
+            http.request(req)
+          end
 
-        case resp
-        when Net::HTTPSuccess
-          hash = JSON.parse(resp.body, symbolize_names: true)
-          parse_Link(resp, hash)
-        else
-          warn "API.call did'nt return Net::HTTPSuccess"
-          pp req.path
-          pp resp
+          case resp
+          when Net::HTTPSuccess
+            hash = JSON.parse(resp.body, symbolize_names: true)
+            parse_Link(resp, hash)
+          else
+            warn "API.call did'nt return Net::HTTPSuccess"
+            pp req.path
+            pp resp
+            $stdout.flush
+            nil
+          end
+        rescue => e
+          error "API.call raise exception"
+          pp e
+          $stdout.flush
           nil
         end
       end
