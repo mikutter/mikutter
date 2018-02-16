1bffbcf28a93c21e4741f645fc42a96f9e0b93d0
cobodo <cobodo@gmail.com>
2018-02-17 02:04:36 +0900

    未収載・非公開・ダイレクトの場合に表示されるsubpartsを追加

--- subparts_visibility.rb
+++ subparts_visibility.rb
@@ -4,7 +4,8 @@ class Gdk::SubPartsWorldonVisibility < Gdk::SubParts
   def get_photo(filename)
     return nil if filename.nil?
     path = Pathname(__dir__) / 'icon' / filename
-    Plugin.filtering(:photo_filter, path.to_s, [])[1].first
+    uri = Diva::URI.new('file://' + path.to_s)
+    Plugin.filtering(:photo_filter, uri, [])[1].first
   end
 
   def filename(visibility)
@@ -23,51 +24,60 @@ class Gdk::SubPartsWorldonVisibility < Gdk::SubParts
 
   def icon_pixbuf
     return nil if !helper.message.respond_to?(:visibility)
-    get_photo(filename(helper.message.visibility))&.pixbuf(width: 20, height: 20)
+    photo = get_photo(filename(helper.message.visibility))
+    photo&.pixbuf(width: @icon_size, height: @icon_size)
   end
 
   def show_icon?
     return nil if !helper.message.respond_to?(:visibility)
-    fn = filename(helper.message)
+    fn = filename(helper.message.visibility)
     !fn.nil?
+  end
+
+  def visibility_text(visibility)
+    case visibility
+    when 'unlisted'
+      '未収載'
+    when 'private'
+      '非公開'
+    when 'direct'
+      'ダイレクト'
+    else
+      ''
+    end
   end
 
   def initialize(*args)
     super
 
     @margin = 2
+    @icon_size = 20
   end
 
   def render(context)
-    # テキスト描画
-    #if helper.visible? && helper.message.respond_to?(:visibility) && helper.message.visibility != 'public'
-    #  context.save do
-    #    context.translate(@margin, 0)
-    #    layout = context.create_pango_layout.tap do |layout|
-    #      layout.font_description = Pango::FontDescription.new(UserConfig[:mumble_basic_font])
-    #      layout.text = helper.message.visibility
-    #    end
-    #    context.set_source_rgb(*(UserConfig[:mumble_basic_color] || [0,0,0]).map{ |c| c.to_f / 65536 })
-    #    context.show_pango_layout(layout)
-    #  end
-    #end
-
-    # 画像描画
     if (helper.visible? && show_icon?)
       pixbuf = icon_pixbuf
-      if pixbuf
-        context.save do
-          context.translate(@margin, 0)
-          context.scale
-          context.set_source_pixbuf(pixbuf)
-          context.paint
-        end
+      if pixbuf.nil?
+        error "SubPartsWorldonVisibility: pixbuf.nil? detected! visibility = #{helper.message.visibility}"
+        return
+      end
+      context.save do
+        context.translate(@margin, @margin)
+        context.set_source_pixbuf(pixbuf)
+        context.paint
+
+        context.translate(@icon_size + @margin, 0)
+        context.set_source_rgb(*(UserConfig[:mumble_basic_color] || [0,0,0]).map{ |c| c.to_f / 65536 })
+        layout = context.create_pango_layout
+        layout.font_description = Pango::FontDescription.new(UserConfig[:mumble_basic_font])
+        layout.text = visibility_text(helper.message.visibility)
+        context.show_pango_layout(layout)
       end
     end
   end
 
   def height
-    @height ||= show_icon? ? 20 : 0
+    @height ||= show_icon? ? @icon_size + 2 * @margin : 0
   end
 end
 
