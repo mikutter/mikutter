04c70378c74033adfcacc9eb86765438d4b73211
Toshiaki Asai <toshi.alternative@gmail.com>
2018-04-24 01:05:25 +0900

    カスタム絵文字のScoreを、正規表現とIndexを用いたループを使わずに実装

--- model/emoji.rb
+++ model/emoji.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 module Plugin::Worldon
   # https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#emoji
   class Emoji < Diva::Model
@@ -6,5 +7,17 @@ module Plugin::Worldon
     field.string :shortcode, required: true
     field.uri :static_url, required: true
     field.uri :url, required: true
+
+    def description
+      ":#{shortcode}:"
+    end
+
+    memoize def inline_photo
+      Enumerator.new{|y| Plugin.filtering(:photo_filter, perma_link, y) }.first
+    end
+
+    def perma_link
+      static_url
+    end
   end
 end
