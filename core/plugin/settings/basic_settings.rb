44c1cc7f71ab4eac05ea0741e60b9ffe2f25f403
Toshiaki Asai <toshi.alternative@gmail.com>
2018-05-06 19:16:23 +0900

    新しいcommitterの名前を入れ忘れていた

--- core/plugin/settings/basic_settings.rb
+++ core/plugin/settings/basic_settings.rb
@@ -59,7 +59,7 @@ Plugin::create(:basic_settings) do
       :license => (file_get_contents('../LICENSE') rescue nil),
       :website => _('http://mikutter.hachune.net/'),
       :logo => Skin.photo('icon.png'),
-      :authors => ['toshi_a', 'Phenomer', 'osa_k', 'katsyoshi'],
+      :authors => ['toshi_a', 'Phenomer', 'osa_k', 'katsyoshi', 'ahiru', 'cobodo'],
       :artists => ['toshi_a', 'soramame_bscl', 'seibe2'],
       :documenters => ['toshi_a']
     }
