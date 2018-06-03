50202e661a368fc0879544fa58d7560bf951b695
Yuto Tokunaga <yuntan.sub1@gmail.com>
2018-06-03 18:07:21 +0900

    extract: 設定画面の抽出タブ一覧にアイコンやピクトを表示 refs #1258

--- plugin/extract/extract_tab_list.rb
+++ plugin/extract/extract_tab_list.rb
@@ -7,24 +7,54 @@ end
 =end
 
 class Plugin::Extract::ExtractTabList < ::Gtk::TreeView
-  ITER_NAME = 0
-  ITER_SLUG = 1
+  ICON_SIZE = 16
+
+  COL_ICON  = 0
+  COL_NAME  = 1
+  COL_SLUG  = 2
+  COL_SOUND = 3
+  COL_POPUP = 4
 
   def initialize(plugin)
-    type_strict plugin => Plugin
-    @plugin = plugin
-    super(Gtk::ListStore.new(String, Symbol))
+    super(Gtk::ListStore.new(
+            GdkPixbuf::Pixbuf, # icon
+            String,            # name
+            Symbol,            # slug
+            GdkPixbuf::Pixbuf, # sound
+            GdkPixbuf::Pixbuf  # popup
+          ))
 
-    append_column Gtk::TreeViewColumn.new(plugin._("名前"), Gtk::CellRendererText.new, text: ITER_NAME)
+    append_column Gtk::TreeViewColumn.new(
+                    '',
+                    Gtk::CellRendererPixbuf.new,
+                    pixbuf: COL_ICON
+                  )
+    append_column Gtk::TreeViewColumn.new(
+                    plugin._('名前'),
+                    Gtk::CellRendererText.new,
+                    text: COL_NAME
+                  ).tap { |col| col.expand = true }
+    append_column Gtk::TreeViewColumn.new(
+                    '',
+                    Gtk::CellRendererPixbuf.new,
+                    pixbuf: COL_SOUND
+                  )
+    append_column Gtk::TreeViewColumn.new(
+                    '',
+                    Gtk::CellRendererPixbuf.new,
+                    pixbuf: COL_POPUP
+                  )
 
-    extract_tabs.each(&method(:add_record)) end
+    extract_tabs.each(&method(:add_record))
+    register_signal_handlers
+  end
 
   # 現在選択されている抽出タブのslugを返す
   # ==== Return
   # 選択されている項目のslug。何も選択されていない場合はnil
   def selected_slug
     selected_iter = selection.selected
-    selected_iter[ITER_SLUG].to_sym if selected_iter
+    selected_iter[COL_SLUG].to_sym if selected_iter
   end
 
   # レコードを追加する
@@ -34,9 +64,18 @@ class Plugin::Extract::ExtractTabList < ::Gtk::TreeView
   # self
   def add_record(record)
     iter = model.append
-    iter[Plugin::Extract::ExtractTabList::ITER_NAME] = record[:name]
-    iter[Plugin::Extract::ExtractTabList::ITER_SLUG] = record[:slug]
+    setup_iter iter, record
     self
+  end
+
+  # レコードをもとにリストビューを更新する
+  # ==== Args
+  # [record] 更新されたレコード(Hash)
+  def update_record(record)
+    update_iter = model.to_enum
+                    .map { |_, _, iter| iter }
+                    .find { |iter| iter[COL_SLUG] == record[:slug] }
+    setup_iter update_iter, record if update_iter
   end
 
   # 抽出タブをリストから削除する
@@ -46,11 +85,23 @@ class Plugin::Extract::ExtractTabList < ::Gtk::TreeView
   # self
   def remove_record(record_slug)
     record_slug = record_slug.to_sym
-    remove_iter = model.to_enum(:each).map{|_,_,iter|iter}.find{|iter| record_slug == iter[ITER_SLUG].to_sym }
+    remove_iter = model.to_enum(:each).map{|_,_,iter|iter}.find{|iter| record_slug == iter[COL_SLUG].to_sym }
     model.remove(remove_iter) if remove_iter
     self end
 
   private
+
+  def register_signal_handlers
+    # 項目をダブルクリックして設定を開く
+    ssc(:button_press_event) do |_, ev|
+      next if ev.event_type != Gdk::Event::BUTTON2_PRESS
+      slug = selected_slug
+      if slug
+        Plugin.call(:extract_open_edit_dialog, slug)
+        true
+      end
+    end
+  end
 
 # ==== utility
 
@@ -61,7 +112,38 @@ class Plugin::Extract::ExtractTabList < ::Gtk::TreeView
     Plugin.filtering(:extract_tabs_get, []).first
   end
 
-# ==== buttons
+  # イテレータにレコードの内容をコピーする
+  # ==== Args
+  # [iter] TreeIter
+  # [record] 参照するレコード (Hash)
+  def setup_iter(iter, record)
+    size = { width: ICON_SIZE, height: ICON_SIZE }
+    set_icon = lambda { |col, photo|
+      iter[col] = photo.load_pixbuf(**size) do |pb|
+        iter[col] = pb unless destroyed?
+      end
+    }
 
+    iter[COL_NAME] = record[:name]
+    iter[COL_SLUG] = record[:slug]
+    if record[:icon]
+      photo = Enumerator.new do |y|
+        Plugin.filtering :photo_filter, record[:icon], y
+      end.first
+      set_icon.call COL_ICON, photo
+    end
+    if record[:sound].to_s.empty?
+      # FIXME: 適当なアイコンを標準スキンに入れる
+      set_icon.call COL_SOUND, Skin['speaker-x.png']
+    else
+      set_icon.call COL_SOUND, Skin['speaker-l.png']
+    end
+    if record[:popup]
+      # FIXME: 適当なアイコンを標準スキンに入れる
+      set_icon.call COL_POPUP, Skin['popup.png']
+    else
+      set_icon.call COL_POPUP, Skin['no-popup.png']
+    end
+  end
 
 end
