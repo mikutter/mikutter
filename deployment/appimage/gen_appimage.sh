7fc5eda49e472bc90df4c3511b2168e2566d6992
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-05-02 04:05:56 +0900

    linuxdeploy.AppImageを使うように変更
    不要な`*.o`ファイルを除去

--- deployment/appimage/gen_appimage.sh
+++ deployment/appimage/gen_appimage.sh
@@ -60,7 +60,7 @@ GEM_HOME=$GEM_DIR GEM_PATH=$GEM_DIR $APP_DIR/usr/bin/ruby $APP_DIR/usr/bin/bundl
 
 echo "--> remove unused files"
 rm -vrf $APP_DIR/usr/share $APP_DIR/usr/include $APP_DIR/usr/lib/{pkgconfig,debug}
-rm -v $APP_DIR/**/*.a
+rm -v $APP_DIR/**/*.{a,o}
 
 echo "--> copy mikutter"
 mkdir -p $APP_DIR/usr/share/mikutter
@@ -68,85 +68,103 @@ cp -av .bundle core mikutter.rb Gemfile LICENSE README $APP_DIR/usr/share/mikutt
 cp -v ~/mikutter $APP_DIR/usr/bin/mikutter
 chmod a+x $APP_DIR/usr/bin/mikutter
 
-echo "--> get helper functions"
-PKG2AICOMMIT=23dd041c0e31f4c63f6e479baf14143cb159b395
-wget -q https://github.com/AppImage/AppImages/raw/${PKG2AICOMMIT}/functions.sh -O ./functions.sh
-. ./functions.sh
+# echo "--> get helper functions"
+# PKG2AICOMMIT=23dd041c0e31f4c63f6e479baf14143cb159b395
+# wget -q https://github.com/AppImage/AppImages/raw/${PKG2AICOMMIT}/functions.sh -O ./functions.sh
+# . ./functions.sh
 
-pushd "$APP_DIR"
+# pushd "$APP_DIR"
 
-echo "--> get AppRun"
+# echo "--> get AppRun"
 # get_apprun
 # use darealshinji/AppImageKit-checkrt's AppRun to exec xdg-open placed
 # outside of the AppImage
 # see https://github.com/darealshinji/AppImageKit-checkrt/pull/11
-wget -q -O $APP_DIR/AppRun https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/AppRun-patched-x86_64
-chmod a+x $APP_DIR/AppRun
-mkdir -p $APP_DIR/usr/optional || true
-wget -q -O $APP_DIR/usr/optional/exec.so https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/exec-x86_64.so
+# wget -q -O $APP_DIR/AppRun https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/AppRun-patched-x86_64
+# chmod a+x $APP_DIR/AppRun
+# mkdir -p $APP_DIR/usr/optional || true
+# wget -q -O $APP_DIR/usr/optional/exec.so https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/exec-x86_64.so
 
-echo "--> get desktop file and icon"
+# echo "--> get desktop file and icon"
 # TODO mikutter.desktopを適当な場所に移動する
 # cp -v $ROOT_DIR/$APP.desktop .
-cp -v ~/mikutter.desktop .
+# cp -v ~/mikutter.desktop .
 # icon should be placed in two place
 # see https://github.com/AppImage/AppImageKit/issues/402
-cp -v $ROOT_DIR/core/skin/data/icon.png $APP.png
-mkdir -p $APP_DIR/usr/share/icons/hicolor/256x256/apps || true
-cp -v $ROOT_DIR/core/skin/data/icon.png $APP_DIR/usr/share/icons/hicolor/256x256/apps/$APP.png
+# cp -v $ROOT_DIR/core/skin/data/icon.png $APP.png
+# mkdir -p $APP_DIR/usr/share/icons/hicolor/256x256/apps || true
+# cp -v $ROOT_DIR/core/skin/data/icon.png $APP_DIR/usr/share/icons/hicolor/256x256/apps/$APP.png
 
-echo "--> get desktop integration"
-set +u
-get_desktopintegration $APP
-set -u
+# echo "--> get desktop integration"
+# set +u
+# get_desktopintegration $APP
+# set -u
 
 echo "--> copy dependencies"
-copy_deps
+# copy_deps
 
 # copy Typelibs for gobject-introspection gem
-cp -av /usr/lib/girepository-* usr/lib
+# cp -av /usr/lib/girepository-* usr/lib
+cp -av /usr/lib/girepository-* $APP_DIR/usr/lib
 
-echo "--> patch away absolute paths"
+# echo "--> patch away absolute paths"
 # for gobject-introspection gem
 # find usr/lib -name libgirepository-1.0.so.1 -exec sed -i -e 's|/usr/lib/girepository-1.0|.////lib/girepository-1.0|g' {} \;
 
-echo "--> move the libraries to usr/lib"
-move_lib
+# echo "--> move the libraries to usr/lib"
+# move_lib
 
-echo "--> delete stuff that should not go into the AppImage."
-set +u
-delete_blacklisted
-set -u
+# echo "--> delete stuff that should not go into the AppImage."
+# set +u
+# delete_blacklisted
+# set -u
 
 # remove libssl and libcrypto
 # see https://github.com/AppImage/AppImageKit/wiki/Desktop-Linux-Platform-Issues#openssl
 # blacklist="libssl.so.1 libssl.so.1.0.0 libcrypto.so.1 libcrypto.so.1.0.0"
-blacklist=
+# blacklist=
 # remove libharfbuzz and it's dependencies,
 # see https://github.com/AppImage/AppImageKit/issues/454
-blacklist=$blacklist" libharfbuzz.so.0 libfreetype.so.6"
-for f in $blacklist; do
-  found="$(find . -name "$f" -not -path "./usr/optional/*")"
-  for f2 in $found; do
-    rm -vf "$f2" "$(readlink -f "$f2")"
-  done
-done
+# blacklist=$blacklist" libharfbuzz.so.0 libfreetype.so.6"
+# for f in $blacklist; do
+#   found="$(find . -name "$f" -not -path "./usr/optional/*")"
+#   for f2 in $found; do
+#     rm -vf "$f2" "$(readlink -f "$f2")"
+#   done
+# done
 
-popd
+# popd
 
-echo "--> enable fuse"
-sudo modprobe fuse
-sudo usermod -a -G fuse $(whoami)
+# echo "--> enable fuse"
+# sudo modprobe fuse
+# sudo usermod -a -G fuse $(whoami)
 
-echo "--> generate AppImage"
+# echo "--> generate AppImage"
 #   - Expects: $ARCH, $APP, $VERSION env vars
 #   - Expects: ./$APP.AppDir/ directory
 #   - Produces: ../out/$APP-$VERSION.glibc$GLIBC_NEEDED-$ARCH.AppImage
-set +u
-generate_type2_appimage
-set -u
+# set +u
+# generate_type2_appimage
+# set -u
 
-echo "--> generated $(ls ../out)"
-mv ../out/*.AppImage* /vagrant
+cp $ROOT_DIR/core/skin/data/icon.png ~/mikutter.png
+
+echo "--> get linuxdeploy"
+wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+chmod +x linuxdeploy-x86_64.AppImage
+# wget -q https://github.com/TheAssassin/linuxdeploy-plugin-checkrt/releases/download/continuous/linuxdeploy-plugin-checkrt-x86_64.sh
+# chmod +x linuxdeploy-plugin-checkrt-x86_64.sh
+
+./linuxdeploy-x86_64.AppImage \
+  --appdir $APP_DIR \
+  --icon-file ~/mikutter.png \
+  --desktop-file ~/mikutter.desktop \
+  --output appimage
+
+# echo "--> generated $(ls ../out)"
+# mv ../out/*.AppImage* /vagrant
+
+echo "--> generated $(ls mikutter*.AppImage)"
+mv mikutter*.AppImage /vagrant
 
 echo '==> finished'
