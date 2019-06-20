666340d8b16c3b40f78afdc9f047a7cf23e39d41
Toshiaki Asai <toshi.alternative@gmail.com>
2019-06-20 20:55:36 +0900

    Revert "libnotify gemでデスクトップ通知を表示する refs #1322"
    This reverts commit eb8b7489312df7d342b6b296d72a91cdcbd55640.

--- core/plugin/libnotify/libnotify.rb
+++ core/plugin/libnotify/libnotify.rb
@@ -1,50 +1,8 @@
 # -*- coding: utf-8 -*-
-# libnotify gemで通知する
+# notify-sendコマンド又はruby-libnotifyで通知を表示する。
 
-require 'libnotify'
-
-Plugin.create :libnotify do
-  on_popup_notify do |user, text, &stop|
-    if text.is_a? Diva::Model
-      text = text.description
-    end
-    notify = Libnotify.new(
-      body: text,
-      summary: user.title,
-      timeout: UserConfig[:notify_expire_time].to_f
-    )
-    notify.update
-    icon_path(user.icon).trap{|err|
-      warn err
-      icon_path(Skin['notfound.png'])
-    }.next{|icon_file_name|
-      notify.icon_path = icon_file_name
-      notify.update
-    }.trap{|err|
-      error err
-      notice "user=#{user.inspect}, text=#{text.inspect}"
-    }
-    stop.call
-  end
-
-  def icon_path(photo)
-    fn = File.join(icon_tmp_dir, Digest::MD5.hexdigest(photo.uri.to_s) + '.png')
-    Delayer::Deferred.new.next{
-      if FileTest.exist?(fn)
-        fn
-      else
-        photo.download_pixbuf(width: 48, height: 48).next{|p|
-          if !FileTest.exist?(fn)
-            FileUtils.mkdir_p(icon_tmp_dir)
-            photo.pixbuf(width: 48, height: 48).save(fn, 'png')
-          end
-          fn
-        }
-      end
-    }
-  end
-
-  memoize def icon_tmp_dir
-    File.join(Environment::TMPDIR, 'libnotify', 'icon').freeze
-  end
+if require_if_exist('RNotify') and defined?(Notify) and Notify.init("mikutter") # ruby-libnotifyがつかえる場合
+  require_relative 'rnotify'
+elsif command_exist? 'notify-send' # notify-sendコマンドが有る場合
+  require_relative 'notify-send'
 end
