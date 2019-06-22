3e4b14f90b240d736ce0f5acbd5d587ba3586207
Toshiaki Asai <toshi.alternative@gmail.com>
2019-06-22 16:10:56 +0900

    よくわからんけどめっちゃ凍ってた

--- core/plugin/quoted_message/quoted_message.rb
+++ core/plugin/quoted_message/quoted_message.rb
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
+# frozen_string_literal: true
 
 Plugin.create :quoted_message do
   # このプラグインが提供するデータソースを返す
   # ==== Return
   # Hash データソース
   def datasources
-    ds = {nested_quoted_myself: _("ナウい引用(全てのアカウント)".freeze)}
+    ds = {nested_quoted_myself: _("ナウい引用(全てのアカウント)")}
     Enumerator.new{|yielder|
       Plugin.filtering(:worlds, yielder)
     }.lazy.select{|world|
       world.class.slug == :twitter
     }.each do |twitter|
-      ds["nested_quote_quotedby_#{twitter.user_obj.id}".to_sym] = "@#{twitter.user_obj.idname}/" + _('ナウい引用'.freeze)
+      ds["nested_quote_quotedby_#{twitter.user_obj.id}".to_sym] = "@#{twitter.user_obj.idname}/" + _('ナウい引用')
     end
     ds end
 
@@ -26,11 +27,11 @@ Plugin.create :quoted_message do
           condition: ->(opt) { opt.messages.all?(&:perma_link) },
           visible: true,
           role: :timeline) do |opt|
-    Gtk::Clipboard.copy(opt.messages.map(&:perma_link).join("\n".freeze))
+    Gtk::Clipboard.copy(opt.messages.map(&:perma_link).join("\n"))
   end
 
   command(:quoted_tweet,
-          name: _('コメント付きリツイート'.freeze),
+          name: _('コメント付きリツイート'),
           icon: Skin[:quote],
           condition: Proc.new{ |opt|
             opt.messages.all?(&:perma_link)},
@@ -38,7 +39,7 @@ Plugin.create :quoted_message do
           role: :timeline) do |opt|
     messages = opt.messages
     opt.widget.create_postbox(to: messages,
-                              footer: ' ' + messages.map(&:perma_link).join(' '.freeze),
+                              footer: ' ' + messages.map(&:perma_link).join(' '),
                               to_display_only: true)
   end
 
@@ -51,7 +52,7 @@ Plugin.create :quoted_message do
       quoted_screen_names = Set.new(
         message.entity.select{ |entity| :urls == entity[:slug] }.map{ |entity|
           matched = Plugin::Twitter::Message::PermalinkMatcher.match(entity[:expanded_url])
-          matched[:screen_name] if matched && matched.names.include?("screen_name".freeze) })
+          matched[:screen_name] if matched && matched.names.include?("screen_name") })
       quoted_services = Enumerator.new{|y|
         Plugin.filtering(:worlds, y)
       }.select{|world|
