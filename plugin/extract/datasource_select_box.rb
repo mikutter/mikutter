92eee0f266c763e191808e08391155fd26a0a1fd
Toshiaki Asai <toshi.alternative@gmail.com>
2020-01-27 23:19:19 +0900

    [extract] コピーできるようになってしまった refs #1433

--- plugin/extract/datasource_select_box.rb
+++ plugin/extract/datasource_select_box.rb
@@ -10,4 +10,29 @@ class Plugin::Extract::DatasourceSelectBox < Gtk::HierarchycalSelectBox
       [id, source_name.is_a?(String) ? source_name.split('/'.freeze) : source_name]
     end
   end
+
+  def menu_pop(widget, _event)
+    contextmenu = Gtk::ContextMenu.new
+    contextmenu.register(_('データソース slugをコピー'), &method(:copy_slug))
+    contextmenu.register(_('subscriberをコピー'), &method(:copy_subscriber))
+    contextmenu.popup(widget, widget)
+  end
+
+  def copy_slug(_optional=nil, _widget=nil)
+    data_sources = self.selection.to_enum(:selected_each).map {|_, _, iter|
+      iter[ITER_ID]
+    }
+    Gtk::Clipboard.copy(data_sources.first.to_s)
+  end
+
+  def copy_subscriber(_optional=nil, _widget=nil)
+    data_sources = self.selection.to_enum(:selected_each).map {|_, _, iter|
+      iter[ITER_ID]
+    }
+    Gtk::Clipboard.copy(<<~'EOM' % {ds: data_sources.first.to_sym.inspect})
+    subscribe(:extract_receive_message, %{ds}).each do |message|
+      
+    end
+    EOM
+  end
 end
