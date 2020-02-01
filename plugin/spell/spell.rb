4e96e81d5b22ca9a12543114e88e74ba3537124d
Toshiaki Asai <toshi.alternative@gmail.com>
2020-02-02 00:23:51 +0900

    search_spell

--- plugin/spell/spell.rb
+++ plugin/spell/spell.rb
@@ -3,6 +3,8 @@ require_relative 'error'
 require_relative 'struct'
 
 Plugin.create(:spell) do
+  defevent :search_spell, prototype: [Pluggaloid::COLLECT, Symbol, [Diva::Model], Hash]
+
   defdsl :defspell do |spell_name, *constraint, condition: nil, &block|
     beh = Plugin::Spell::Spell.new(spell_name.to_sym,
                                   Set.new(constraint).freeze,
@@ -26,22 +28,18 @@ Plugin.create(:spell) do
     optional = {}.freeze
     models = models.compact
     *models, optional = models unless models.last.is_a?(Diva::Model)
-    Delayer::Deferred.new.next{
-      Enumerator.new{|y|
-        Plugin.filtering(:search_spell, y, name.to_sym, models, optional)
-      }.first
-    }.next{|spell|
+    Delayer::Deferred.new.next {
+      Plugin.collect(:search_spell, name.to_sym, models, optional).first
+    }.next do |spell|
       raise Plugin::Spell::SpellNotFoundError, "Spell `#{name}' (#{models.map(&:class).join(', ')}) does not exists." unless spell
       spell.call(models, optional)
-    }
+    end
   end
 
   defdsl :spell? do |name, *models|
     optional = {}.freeze
     models = models.compact
     *models, optional = models unless models.last.is_a?(Diva::Model)
-    !Enumerator.new{|y|
-      Plugin.filtering(:search_spell, y, name.to_sym, models, optional)
-    }.take(1).to_a.empty?
+    !Plugin.collect(:search_spell, name.to_sym, models, optional).first(1).empty?
   end
 end
