2b9f01c41eb1a8b299cadf89952ad9d2a1143bdc
Toshiaki Asai <toshi.alternative@gmail.com>
2020-05-05 13:19:55 +0900

    bought huge monitor refs #1420

--- plugin/pulseaudio/pulseaudio.rb
+++ plugin/pulseaudio/pulseaudio.rb
@@ -14,7 +14,11 @@ Plugin.create(:pulseaudio) do
           error err
           pacmd.close
           notice 'Retry to play sound.'
-          pacmd.puts "play-file #{filename} #{default_sink}" end end end end
+          pacmd.puts "play-file #{filename} #{default_sink}"
+        end
+      end
+    end
+  end
 
   # pacmd コマンドを実行して、書き込み用IOを返す。既に書き込み用IOが開いているならそちらを返す。
   # 既にcloseされているなら新しく接続しなおして返す。
@@ -26,22 +30,27 @@ Plugin.create(:pulseaudio) do
     else
       notice 'connection refused or not connected. reconnecting...'
       @default_sink = nil
-      @pacmd = IO.popen('pacmd', File::Constants::WRONLY).tap{ |pacmd|
+      @pacmd = IO.popen('pacmd', File::Constants::WRONLY).tap do |pacmd|
         notice 'connected.'
         pacmd.close_on_exec = true
         pacmd.autoclose = true
-        pacmd.sync = true } end end
+        pacmd.sync = true
+      end
+    end
+  end
 
   # PulseAudioに設定されている Default Sinkの名前を返す
   # ==== Return
   # String デフォルトシンクの名前
   def default_sink
-    @default_sink ||= Open3.popen2({'LC_ALL' => 'C'} ,'pactl info'){ |input, output, _|
+    @default_sink ||= Open3.popen2({'LC_ALL' => 'C'} ,'pactl info') do |input, output, _|
       input.close
       target_line = output.find{ |line| line.start_with?('Default Sink') }
       if target_line
         result = target_line.match(/^Default Sink\s*:\s*(.+)$/)[1]
         notice "detected default sink: #{result}"
-        result end } end
-
+        result
+      end
+    end
+  end
 end
