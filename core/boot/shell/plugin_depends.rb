ba93640909f5f875bdbf021eec7e811f40fe63f3
Toshiaki Asai <toshi.alternative@gmail.com>
2020-10-07 22:17:15 +0900

    refactoring refs #1477

--- core/boot/shell/plugin_depends.rb
+++ core/boot/shell/plugin_depends.rb
@@ -10,13 +10,21 @@ Environment::PLUGIN_PATH.each do |path|
 end
 Miquire::Plugin.loadpath << File.join(Environment::CONFROOT, 'plugin')
 
-writer = lambda do |spec|
+escape = -> (v) do
+  if /[^\w]/.match?(v)
+    %<"#{v}">
+  else
+    v
+  end
+end
+
+writer = -> (spec) do
   depends = Miquire::Plugin.depended_plugins(spec)
-  if (depends or []).empty?
-    puts "  #{spec[:slug]};"
+  if (depends || []).empty?
+    puts "  #{escape.(spec[:slug])};"
   else
     depends.each do |depend|
-      puts "  #{spec[:slug]} -> #{depend[:slug]};"
+      puts "  #{escape.(spec[:slug])} -> #{escape.(depend[:slug])};"
     end
   end
 end
@@ -26,9 +34,11 @@ puts 'digraph mikutter_plugin {'
 if Array(Mopt.plugin).empty?
   Miquire::Plugin.each_spec(&writer)
 else
-  available = Array(Mopt.plugin).inject(Set.new(Array(Mopt.plugin))){|depends, depend_slug|
-    depends + Miquire::Plugin.depended_plugins(depend_slug, recursive: true).
-      map{|spec| spec[:slug] }}
+  available = Array(Mopt.plugin).inject(Set.new(Array(Mopt.plugin))) do |depends, depend_slug|
+    Miquire::Plugin.depended_plugins(depend_slug, recursive: true).each do |spec|
+      depends << spec[:slug]
+    end
+  end
   available.map{|x| x.to_spec }.each(&writer)
 end
 
